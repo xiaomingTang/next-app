d4fd8470950231ce4b0931635b4c80d6a3b74b1c
xiaomingTang <1038761793@qq.com>
2023-05-29 23:08:20 +0800

    feat: 新增 seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,16 @@ import './globals.css'
 import Contexts from '@/common/contexts'
 import Polyfills from '@/common/polyfills'
 import Providers from '@/common/providers'
+import { seo } from '@/utils/seo'
 
 import clsx from 'clsx'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'local next app',
-  description: 'next app test',
-}
+export const metadata = seo.defaults({
+  title: 'home',
+})
 
 export default function RootLayout({
   children,
@@ -23,10 +23,6 @@ export default function RootLayout({
   return (
     <html lang='zh-cn' data-theme='dark'>
       <head>
-        <meta
-          name='format-detection'
-          content='telephone=no, date=no, email=no, address=no'
-        />
         <script src='/__ENV_CONFIG__.js' />
       </head>
       <body className={clsx(inter.className, 'min-h-screen')}>
---
+++ src/utils/seo.ts
@@ -0,0 +1,70 @@
+import { ENV_CONFIG } from '@/config'
+
+import type { Metadata } from 'next'
+
+const { manifest } = ENV_CONFIG
+
+interface SeoProps {
+  title?: string
+  description?: string
+  keywords?: string
+}
+
+export const seo = {
+  title: (customTitle = '', asFullTitle = false) => {
+    const inputTitle = customTitle.trim()
+    if (!inputTitle) {
+      return manifest.name
+    }
+    if (asFullTitle) {
+      return inputTitle
+    }
+    return `${inputTitle} | ${manifest.name}`
+  },
+  description: (customDescription = '', asFullDescription = false) => {
+    const inputDescription = customDescription.trim()
+    if (!inputDescription) {
+      return manifest.description
+    }
+    if (asFullDescription) {
+      return inputDescription
+    }
+    return `${inputDescription} | ${manifest.description}`
+  },
+  /**
+   * keywords 间以英文 逗号 分隔
+   */
+  keywords: (customKeywords = '', asFullKeywords = false) => {
+    const inputKeywords = customKeywords.trim()
+    if (!inputKeywords) {
+      return manifest.keywords
+    }
+    if (asFullKeywords) {
+      return inputKeywords
+    }
+    return `${inputKeywords}, ${manifest.keywords}`
+  },
+  defaults: (props?: SeoProps): Metadata => ({
+    metadataBase: new URL(ENV_CONFIG.public.origin),
+    title: seo.title(props?.title),
+    description: seo.description(props?.description),
+    keywords: seo.keywords(props?.keywords),
+    generator: 'Next.js',
+    themeColor: [
+      { media: '(prefers-color-scheme: dark)', color: '#1a2027' },
+      { media: '(prefers-color-scheme: light)', color: '#eeeeee' },
+    ],
+    viewport: { width: 'device-width', initialScale: 1 },
+    icons: manifest.icons,
+    manifest: `/manifest.json`,
+    formatDetection: {
+      address: false,
+      telephone: false,
+      email: false,
+    },
+    appleWebApp: {
+      capable: true,
+      title: manifest.short_name,
+    },
+  }),
+}
