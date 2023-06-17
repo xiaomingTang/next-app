9b83436e97d1c0ef2b49a9331508f8648df5c5e9
xiaomingTang <1038761793@qq.com>
2023-06-18 06:26:57 +0800

    feat: 新增阅读进度 & 滚动到顶部

--- src/app/ClientComp.tsx
+++ src/app/ClientComp.tsx
@@ -1,17 +1,68 @@
 'use client'
 
+import { useElementScroll } from '@/hooks/useElementScroll'
 import { useUser } from '@/user'
 
+import { KeyboardArrowUp } from '@mui/icons-material'
+import {
+  Box,
+  CircularProgress,
+  Fab,
+  Fade,
+  circularProgressClasses,
+} from '@mui/material'
 import { usePathname } from 'next/navigation'
+import { useRef } from 'react'
 
 export function Comp() {
   const pathname = usePathname()
   const user = useUser()
+  const elemRef = useRef<HTMLDivElement>(null)
+  const scrollStarterRef = useRef<HTMLDivElement>(null)
+  const { percent } = useElementScroll({ elem: elemRef })
+
   return (
     <>
       <p>pathname: {pathname}</p>
       <p>user: {user.name}</p>
-      <div style={{ height: '200vh' }}></div>
+      <div style={{ height: '50vh' }}></div>
+      <div style={{ height: '150vh', background: '#bbbbbb' }} ref={elemRef}>
+        <div
+          ref={scrollStarterRef}
+          className='-translate-y-10 md:-translate-y-14'
+        />
+      </div>
+      <div style={{ height: '100vh' }}></div>
+
+      <Fade in={percent > 0}>
+        <Box sx={{ position: 'fixed', bottom: 48, right: 16 }}>
+          <CircularProgress
+            variant='determinate'
+            size={46}
+            color='primary'
+            sx={{
+              position: 'absolute',
+              top: -3,
+              left: -3,
+              [`& .${circularProgressClasses.circle}`]: {
+                strokeLinecap: 'round',
+              },
+            }}
+            value={percent * 100}
+          />
+          <Fab
+            size='small'
+            aria-label='scroll back to top'
+            onClick={() => {
+              scrollStarterRef.current?.scrollIntoView({
+                behavior: 'smooth',
+              })
+            }}
+          >
+            <KeyboardArrowUp />
+          </Fab>
+        </Box>
+      </Fade>
     </>
   )
 }
---
+++ src/hooks/useElementScroll.ts
@@ -0,0 +1,45 @@
+import { clamp, throttle } from 'lodash-es'
+import { useEffect, useState } from 'react'
+
+import type { RefObject } from 'react'
+
+interface Props {
+  elem: HTMLElement | RefObject<HTMLElement>
+  scrollElem?: HTMLElement | RefObject<HTMLElement>
+}
+
+/**
+ * @returns 该元素滚动过的 y 方向上的百分比
+ */
+export function useElementScroll({ elem, scrollElem }: Props) {
+  const [percent, setPercent] = useState(0)
+
+  useEffect(() => {
+    const realScrollElem =
+      (scrollElem instanceof HTMLElement ? scrollElem : scrollElem?.current) ??
+      window
+
+    const onScroll = throttle(
+      () => {
+        const realElem = elem instanceof HTMLElement ? elem : elem.current
+        if (!realElem) {
+          return
+        }
+        const rect = realElem.getBoundingClientRect()
+        setPercent(clamp(rect.top / (window.innerHeight - rect.height), 0, 1))
+      },
+      300,
+      {
+        leading: false,
+        trailing: true,
+      }
+    )
+    realScrollElem.addEventListener('scroll', onScroll)
+
+    return () => {
+      realScrollElem.removeEventListener('scroll', onScroll)
+    }
+  }, [elem, scrollElem])
+
+  return { percent }
+}
