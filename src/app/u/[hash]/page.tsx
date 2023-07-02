880dc133e12f2d5fbb045b7774daf6a734616790
xiaomingTang <1038761793@qq.com>
2023-07-03 00:36:35 +0800

    feat: 新增短链前台页面

---
+++ src/app/u/[hash]/page.tsx
@@ -0,0 +1,32 @@
+import { getShortUrl } from './server'
+
+import { Error } from '@/components/Error'
+import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
+import DefaultLayout from '@/layout/DefaultLayout'
+import { seo } from '@/utils/seo'
+
+import { redirect, notFound } from 'next/navigation'
+
+interface Props {
+  params: { hash: string }
+}
+
+export const metadata = seo.defaults({
+  title: '短链服务',
+})
+
+export default async function Index({ params: { hash } }: Props) {
+  const { data, error } = await getShortUrl({
+    hash,
+  })
+  if (error) {
+    return (
+      <DefaultLayout>
+        <DefaultBodyContainer>
+          <Error {...error} />
+        </DefaultBodyContainer>
+      </DefaultLayout>
+    )
+  }
+  return redirect(data.url)
+}
