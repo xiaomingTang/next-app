966ff89f02660f76c75b56acb9c4a43633a7824e
xiaomingTang <1038761793@qq.com>
2023-07-03 00:35:42 +0800

    feat: seo

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,8 +4,13 @@ import DefaultLayout from '@/layout/DefaultLayout'
 import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
 import { getBlogs } from '@/app/admin/blog/components/server'
 import { SA } from '@/errors/utils'
+import { seo } from '@/utils/seo'
 
 import { BlogType } from '@prisma/client'
+
+export const metadata = seo.defaults({
+  title: '博客列表页',
+})
 
 export default async function Home() {
   const blogs = await getBlogs({
