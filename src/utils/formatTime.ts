b79fc25140336d88a42e3e9c6f17d76a96d0567c
xiaomingTang <1038761793@qq.com>
2023-07-05 21:02:42 +0800

    feat: 修改超过一周后的显示逻辑

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -7,11 +7,13 @@ function padTwo(s: string | number) {
  * 一小时内, 显示 "一小时内"
  * 一天内, 显示 "xx 小时前"
  * 一周内, 显示 "xx 天前"
- * 一周以上, 显示 "xxxx-xx-xx"
+ * 一周以上, 同一年, 显示 "xx-xx"
+ * 一周以上, 不同年, 显示 "xxxx-xx-xx"
  */
 export function friendlyFormatTime(time: number | string | Date) {
   const date = time instanceof Date ? time : new Date(time)
-  const diff = Date.now() - date.getTime()
+  const nowDate = new Date()
+  const diff = nowDate.getTime() - date.getTime()
   const hour = 60 * 60 * 1000
   if (diff < hour) {
     return '一小时内'
@@ -21,6 +23,9 @@ export function friendlyFormatTime(time: number | string | Date) {
   }
   if (diff < 7 * 24 * hour) {
     return `${Math.floor(diff / (24 * hour))}天前`
+  }
+  if (nowDate.getFullYear() === date.getFullYear()) {
+    return `${padTwo(date.getMonth() + 1)}-${padTwo(date.getDate())}`
   }
   return `${date.getFullYear()}-${padTwo(date.getMonth() + 1)}-${padTwo(
     date.getDate()
