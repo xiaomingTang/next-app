9b37054108a03a04b852f49ad7cc7393a7aa062c
xiaomingTang <1038761793@qq.com>
2023-07-20 00:34:24 +0800

    fix: writeEnvVars 判断条件写错了

--- bin/runtime-scripts/pre-run.js
+++ bin/runtime-scripts/pre-run.js
@@ -74,10 +74,10 @@ export {};
 }
 
 function main() {
-  writeEnvVars()
   if (process.env.WRITE_DOT_ENV === 'true') {
-    writeEnvConfig(process.env.NEXT_PUBLIC_APP_ENV)
+    writeEnvVars()
   }
+  writeEnvConfig(process.env.NEXT_PUBLIC_APP_ENV)
 }
 
 main()
