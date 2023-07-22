2a88bbb60e49573ac6ff6fe6db39a87845082a55
xiaomingTang <1038761793@qq.com>
2023-07-23 04:52:23 +0800

    feat: 新增返回值类型为待校验类型

--- src/request/validator.ts
+++ src/request/validator.ts
@@ -2,7 +2,7 @@ import { validate } from 'jsonschema'
 import Boom from '@hapi/boom'
 
 import type { Options, Schema, ValidationError } from 'jsonschema'
-import type { TSchema } from '@sinclair/typebox'
+import type { Static, TSchema } from '@sinclair/typebox'
 
 interface ValidatorResultError extends Error {
   name: 'Validation Error'
@@ -16,11 +16,15 @@ function formatValidationError(error: ValidatorResultError) {
   return error.errors.map((err) => err.toString()).join('; ')
 }
 
-export function validateRequest<T extends TSchema>(dto: T, data: unknown) {
+export function validateRequest<T extends TSchema>(
+  dto: T,
+  data: unknown
+): Static<T> {
   try {
     validate(data, dto, {
       throwAll: true,
     })
+    return data as Static<T>
   } catch (error) {
     throw Boom.badRequest(formatValidationError(error as ValidatorResultError))
   }
