90e5c94b8da3ff8a9f3f5824fb42818d9be9885d
xiaomingTang <1038761793@qq.com>
2023-07-23 06:28:03 +0800

    feat: upload 页前端包裹 AuthRequired

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,5 +1,7 @@
 import { Uploader } from './components/Uploader'
 
+import { Forbidden } from '@/components/Forbidden'
+import { AuthRequired } from '@/components/AuthRequired'
 import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
 import DefaultLayout from '@/layout/DefaultLayout'
 import { seo } from '@/utils/seo'
@@ -12,7 +14,9 @@ export default async function Index() {
   return (
     <DefaultLayout>
       <DefaultBodyContainer>
-        <Uploader />
+        <AuthRequired fallback={<Forbidden />}>
+          <Uploader />
+        </AuthRequired>
       </DefaultBodyContainer>
     </DefaultLayout>
   )
