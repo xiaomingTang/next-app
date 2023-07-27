5c2468f30e5d8d1ef76b18c83d52dcbf87697371
xiaomingTang <1038761793@qq.com>
2023-07-28 01:08:39 +0800

    feat: sleepMs 加输入值校验

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,7 +1,14 @@
+import { InfiniteTimeout } from '@/constants'
+
+import { clamp } from 'lodash-es'
+
 export async function sleepMs(ms: number): Promise<void> {
+  if (Number.isNaN(ms)) {
+    throw new Error('invalid input: NaN')
+  }
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve()
-    }, ms)
+    }, clamp(ms, 0, InfiniteTimeout))
   })
 }
