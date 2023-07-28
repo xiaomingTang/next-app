0c6540ba031bbf9166668a3e57bbe897c54a4cf9
xiaomingTang <1038761793@qq.com>
2023-07-29 00:39:28 +0800

    feat: 401 时默认展示登录弹窗

--- src/errors/catchAndToast.ts
+++ src/errors/catchAndToast.ts
@@ -2,24 +2,41 @@
 
 import { toError } from './utils'
 
+import { useUser } from '@/user'
+
 import { toast } from 'react-hot-toast'
+import httpStatus from 'http-status'
+import { noop } from 'lodash-es'
 
 import type { Func } from './utils'
 
 type AwaitedValue<T> = T extends Promise<infer S> ? S : T
 
+interface CatProps {
+  doNotShowLoginModalWhen401?: boolean
+}
+
 /**
  * catch and toast
  */
 export function cat<Args extends unknown[], Ret>(
-  callback: Func<Args, Ret>
+  callback: Func<Args, Ret>,
+  props?: CatProps
 ): Func<Args, Promise<AwaitedValue<Ret> | undefined>> {
   return async (...args) => {
     try {
       const ret = (await callback(...args)) as AwaitedValue<Ret>
       return ret
     } catch (catchError) {
-      toast.error(toError(catchError).message ?? '服务器错误，请稍后再试')
+      const error = toError(catchError)
+      if (
+        error.code === httpStatus.UNAUTHORIZED &&
+        props?.doNotShowLoginModalWhen401 !== true
+      ) {
+        // 不关心完成与否, 所以无需 await
+        useUser.login().catch(noop)
+      }
+      toast.error(error.message ?? '服务器错误，请稍后再试')
       return undefined
     }
   }
