17e8f5ea6af4b743578f99bf3ffd1329a0cf76ad
xiaomingTang <1038761793@qq.com>
2023-07-30 01:09:40 +0800

    feat: 重写上传功能，做成 dialog

--- src/app/upload/components/Uploader.tsx
+++ src/app/upload/components/Uploader.tsx
@@ -5,36 +5,47 @@ import { deleteFile, uploadFiles } from '../server'
 import { SA, toPlainError } from '@/errors/utils'
 import { cat } from '@/errors/catchAndToast'
 import { CustomLoadingButton } from '@/components/CustomLoadingButton'
+import { SlideUpTransition } from '@/components/SlideUpTransition'
 
 import WarningIcon from '@mui/icons-material/Warning'
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom'
 import UploadFileIcon from '@mui/icons-material/UploadFile'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
-import ExpandLessIcon from '@mui/icons-material/ExpandLess'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import CloseIcon from '@mui/icons-material/Close'
+import SettingsIcon from '@mui/icons-material/Settings'
+import CopyAllIcon from '@mui/icons-material/CopyAll'
 import {
   TextField,
   Checkbox,
   FormControlLabel,
   FormControl,
-  FormHelperText,
   Stack,
   List,
   ListItem,
-  ListItemText,
   IconButton,
   ButtonGroup,
   Button,
-  Collapse,
-  ListItemButton,
   Tooltip,
   Typography,
+  Dialog,
+  DialogContent,
+  Menu,
+  AppBar,
+  Toolbar,
+  Box,
+  DialogActions,
+  useMediaQuery,
+  useTheme,
+  Divider,
+  ListItemAvatar,
+  Avatar,
 } from '@mui/material'
 import { Controller, useForm } from 'react-hook-form'
 import { useCallback, useMemo, useState } from 'react'
 import { omit } from 'lodash-es'
 import { toast } from 'react-hot-toast'
 import CopyToClipboard from 'react-copy-to-clipboard'
+import NiceModal, { muiDialogV5, useModal } from '@ebay/nice-modal-react'
 
 interface FileInfo {
   file: File
@@ -53,6 +64,10 @@ interface FileInfo {
   error?: string
 }
 
+interface UploaderProps {
+  onSuccess?: (fileInfos: FileInfo[]) => void
+}
+
 function geneKey({ file }: FileInfo) {
   return `${file.name} - ${file.size} - ${file.type} - ${file.lastModified}`
 }
@@ -68,6 +83,10 @@ const uploadStatusMap: Record<
     title: '待上传',
     icon: <UploadFileIcon color='info' />,
   },
+  pending: {
+    title: '上传中',
+    icon: <HourglassBottomIcon color='disabled' />,
+  },
   succeed: {
     title: '上传成功',
     icon: <CheckCircleIcon color='success' />,
@@ -76,15 +95,99 @@ const uploadStatusMap: Record<
     title: '上传失败',
     icon: <WarningIcon color='error' />,
   },
-  pending: {
-    title: '上传中',
-    icon: <HourglassBottomIcon color='disabled' />,
-  },
+}
+
+function FileInfoDisplay({
+  onDelete,
+  index,
+  ...info
+}: FileInfo & {
+  index: number
+  onDelete: () => Promise<void>
+}) {
+  const isImage = info.file.type.startsWith('image')
+  return (
+    <>
+      {index !== 0 && (
+        <Divider key={`divider-${index}`} variant='inset' component='li' />
+      )}
+      <ListItem
+        alignItems='flex-start'
+        sx={{ pr: '96px' }}
+        secondaryAction={
+          <>
+            {info.status !== 'pending' && (
+              <CustomLoadingButton
+                aria-label='删除该文件'
+                color='error'
+                size='small'
+                onClick={onDelete}
+              >
+                删除
+              </CustomLoadingButton>
+            )}
+            {info.status === 'failed' ? (
+              <Tooltip
+                title={`${uploadStatusMap[info.status].title}: ${
+                  info.error ?? ''
+                }`}
+              >
+                <IconButton
+                  edge='end'
+                  aria-label={uploadStatusMap[info.status].title}
+                >
+                  {uploadStatusMap[info.status].icon}
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <IconButton
+                edge='end'
+                aria-label={uploadStatusMap[info.status].title}
+                disabled
+              >
+                {uploadStatusMap[info.status].icon}
+              </IconButton>
+            )}
+          </>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar alt={info.file.name} src='' />
+        </ListItemAvatar>
+        <Stack sx={{ flex: '1 1 auto', overflow: 'hidden' }}>
+          <Typography
+            noWrap
+            color={info.status === 'failed' ? 'error' : undefined}
+          >
+            {info.file.name}
+          </Typography>
+          {info.url && (
+            <ButtonGroup size='small' variant='outlined'>
+              <CopyToClipboard
+                text={info.url}
+                onCopy={() => toast.success('复制成功')}
+              >
+                <Button>复制 url</Button>
+              </CopyToClipboard>
+              <CopyToClipboard
+                text={`${isImage ? '!' : ''}[${info.file.name}](${info.url})`}
+                onCopy={() => toast.success('复制成功')}
+              >
+                <Button>复制 markdown 格式</Button>
+              </CopyToClipboard>
+            </ButtonGroup>
+          )}
+        </Stack>
+      </ListItem>
+    </>
+  )
 }
 
 // TODO: 做成弹窗形式 & 简易上传弹窗 & 全屏/轻量/单个文件上传 等
-export function Uploader() {
-  const [open, setOpen] = useState(false)
+const Uploader = NiceModal.create(({ onSuccess }: UploaderProps) => {
+  const modal = useModal()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const fullScreen = useMediaQuery(useTheme().breakpoints.down('sm'))
   const { handleSubmit, control } = useForm<{
     private: boolean
     randomFilenameByServer: boolean
@@ -182,14 +285,8 @@ export function Uploader() {
     <Controller
       name='dirname'
       control={control}
-      render={({ field, fieldState: { error } }) => (
-        <TextField
-          {...field}
-          size='small'
-          label='存储目录(可缺省)'
-          helperText={error?.message ?? ' '}
-          error={!!error}
-        />
+      render={({ field }) => (
+        <TextField {...field} size='small' label='存储目录(可缺省)' />
       )}
     />
   )
@@ -198,13 +295,12 @@ export function Uploader() {
     <Controller
       name='private'
       control={control}
-      render={({ field, fieldState: { error } }) => (
-        <FormControl error={!!error} size='small'>
+      render={({ field }) => (
+        <FormControl size='small'>
           <FormControlLabel
             control={<Checkbox size='small' checked={field.value} {...field} />}
             label='私密文件'
           />
-          <FormHelperText>{error?.message ?? ' '}</FormHelperText>
         </FormControl>
       )}
     />
@@ -214,168 +310,151 @@ export function Uploader() {
     <Controller
       name='randomFilenameByServer'
       control={control}
-      render={({ field, fieldState: { error } }) => (
-        <FormControl error={!!error} size='small'>
+      render={({ field }) => (
+        <FormControl size='small'>
           <FormControlLabel
             control={<Checkbox size='small' checked={field.value} {...field} />}
             label='随机文件名（服务器随机生成）'
           />
-          <FormHelperText>{error?.message ?? ' '}</FormHelperText>
         </FormControl>
       )}
     />
   )
 
-  const filesDisplayElem = (
+  const settingMenu = (
+    <Menu
+      id='logout-menu'
+      anchorEl={anchorEl}
+      open={!!anchorEl}
+      autoFocus
+      onClose={() => setAnchorEl(null)}
+      MenuListProps={{
+        'aria-labelledby': '关闭设置菜单',
+      }}
+    >
+      <Stack spacing={1} sx={{ p: 1 }}>
+        {dirnameElem}
+        {privateElem}
+        {randomFilenameElem}
+      </Stack>
+    </Menu>
+  )
+
+  const header = (
     <>
-      {fileInfos.map((info) => (
-        <ListItem
-          key={geneKey(info)}
-          secondaryAction={
-            <>
-              {info.status !== 'pending' && (
-                <CustomLoadingButton
-                  aria-label='删除该文件'
-                  color='error'
-                  size='small'
-                  onClick={async () => {
-                    if (info.key) {
-                      await deleteFile(info.key)
-                    }
-                    setFileInfoMap((prev) => omit(prev, [geneKey(info)]))
-                  }}
-                >
-                  删除
-                </CustomLoadingButton>
-              )}
-              {info.status === 'failed' ? (
-                <Tooltip
-                  title={`${uploadStatusMap[info.status].title}: ${
-                    info.error ?? ''
-                  }`}
-                >
-                  <IconButton
-                    edge='end'
-                    aria-label={uploadStatusMap[info.status].title}
-                  >
-                    {uploadStatusMap[info.status].icon}
-                  </IconButton>
-                </Tooltip>
-              ) : (
-                <IconButton
-                  edge='end'
-                  aria-label={uploadStatusMap[info.status].title}
-                  disabled
-                >
-                  {uploadStatusMap[info.status].icon}
-                </IconButton>
-              )}
-            </>
-          }
-        >
-          {info.url ? (
-            <CopyToClipboard
-              text={info.url}
-              onCopy={() => toast.success('链接复制成功')}
+      {settingMenu}
+      <AppBar sx={{ position: 'relative' }}>
+        <Toolbar variant='dense'>
+          <Box sx={{ flex: 1 }}>
+            <IconButton
+              edge='start'
+              aria-label='设置'
+              onClick={(e) => {
+                setAnchorEl(e.currentTarget)
+              }}
             >
-              <ListItemText
-                primary={
-                  <Typography
-                    color={info.status === 'failed' ? 'error' : undefined}
-                  >
-                    {info.file.name}
-                  </Typography>
-                }
-                sx={{ cursor: 'copy' }}
-              />
+              <SettingsIcon />
+            </IconButton>
+            <CopyToClipboard
+              text={fileInfos
+                .filter((info) => info.status === 'succeed')
+                .map((info) => info.url ?? '')
+                .join('\n')}
+              onCopy={() => toast.success('复制成功')}
+            >
+              <IconButton aria-label='复制所有'>
+                <CopyAllIcon />
+              </IconButton>
             </CopyToClipboard>
-          ) : (
-            <ListItemText
-              primary={
-                <Typography
-                  color={info.status === 'failed' ? 'error' : undefined}
-                >
-                  {info.file.name}
-                </Typography>
-              }
-            />
-          )}
-        </ListItem>
-      ))}
+          </Box>
+          <IconButton
+            edge='end'
+            aria-label='取消编辑'
+            onClick={() => {
+              modal.hide()
+              onSuccess?.(fileInfos.filter((info) => info.status === 'succeed'))
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </>
   )
 
-  const filesActionElem = (
-    <ListItem key='actions'>
-      <ButtonGroup fullWidth>
-        <Button fullWidth tabIndex={-1}>
-          添加文件
-          <input
-            type='file'
-            style={{
-              opacity: 0,
-              position: 'absolute',
-              left: 0,
-              top: 0,
-              right: 0,
-              bottom: 0,
-              cursor: 'pointer',
-            }}
-            multiple
-            onInput={(e) => {
-              const target = e.target as HTMLInputElement
-              const files = Array.from(target.files ?? [])
-              const fileToInfo = (f: File): FileInfo => ({
-                file: f,
-                status: 'before-upload',
-              })
-              updateFileInfos(files.map(fileToInfo), false)
-            }}
-          />
-        </Button>
-        <CustomLoadingButton fullWidth variant='contained' onClick={onSubmit}>
-          立即上传
-        </CustomLoadingButton>
-      </ButtonGroup>
-    </ListItem>
+  const filesDisplayElem = (
+    <List>
+      {fileInfos.map((info, index) => (
+        <FileInfoDisplay
+          key={geneKey(info)}
+          {...info}
+          index={index}
+          onDelete={async () => {
+            if (info.key) {
+              await deleteFile(info.key)
+            }
+            setFileInfoMap((prev) => omit(prev, [geneKey(info)]))
+          }}
+        />
+      ))}
+    </List>
+  )
+
+  const actionsElem = (
+    <>
+      <Button variant='outlined' tabIndex={-1}>
+        添加文件
+        <input
+          type='file'
+          style={{
+            opacity: 0,
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            cursor: 'pointer',
+          }}
+          multiple
+          onInput={(e) => {
+            const target = e.target as HTMLInputElement
+            const files = Array.from(target.files ?? [])
+            const fileToInfo = (f: File): FileInfo => ({
+              file: f,
+              status: 'before-upload',
+            })
+            updateFileInfos(files.map(fileToInfo), false)
+          }}
+        />
+      </Button>
+      <CustomLoadingButton variant='contained' onClick={onSubmit}>
+        立即上传
+      </CustomLoadingButton>
+    </>
   )
 
   return (
-    <>
-      {/* 高级配置 */}
-      <ListItem
-        disablePadding
-        secondaryAction={
-          <IconButton tabIndex={-1} onClick={() => setOpen((prev) => !prev)}>
-            {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </IconButton>
-        }
-        sx={{ bgcolor: 'background.paper' }}
-      >
-        <ListItemButton onClick={() => setOpen((prev) => !prev)}>
-          <ListItemText primary='高级配置' />
-        </ListItemButton>
-      </ListItem>
-      <Collapse in={open} timeout='auto' sx={{ bgcolor: 'background.paper' }}>
-        <Stack spacing={1} direction='column' sx={{ p: 2 }}>
-          {dirnameElem}
-          {privateElem}
-          {randomFilenameElem}
-        </Stack>
-      </Collapse>
-      {/* 文件列表 */}
-      <List
-        sx={{
-          width: '100%',
-          maxWidth: '400px',
-          mx: 'auto',
-          mt: 2,
-          bgcolor: 'background.paper',
-        }}
-      >
-        {filesDisplayElem}
-        {filesActionElem}
-      </List>
-    </>
+    <Dialog
+      fullWidth
+      fullScreen={fullScreen}
+      TransitionComponent={SlideUpTransition}
+      {...muiDialogV5(modal)}
+      onClose={() => {
+        modal.hide()
+        onSuccess?.(fileInfos.filter((info) => info.status === 'succeed'))
+      }}
+    >
+      {header}
+      <DialogContent>{filesDisplayElem}</DialogContent>
+      <DialogActions>{actionsElem}</DialogActions>
+    </Dialog>
   )
+})
+
+export async function upload(): Promise<FileInfo[]> {
+  return new Promise<FileInfo[]>((resolve) => {
+    NiceModal.show(Uploader, {
+      onSuccess: resolve,
+    })
+  })
 }
