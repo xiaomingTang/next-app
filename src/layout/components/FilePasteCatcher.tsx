960cd2a42216de93c02f3c22a80ed499d30ea4c9
xiaomingTang <1038761793@qq.com>
2023-07-30 18:44:28 +0800

    fix: 粘贴时过滤掉文件夹

--- src/layout/components/FilePasteCatcher.tsx
+++ src/layout/components/FilePasteCatcher.tsx
@@ -7,7 +7,10 @@ import { useEffect } from 'react'
 export function FilePasteCatcher() {
   useEffect(() => {
     const onPaste = (e: ClipboardEvent) => {
-      const files = Array.from(e.clipboardData?.files ?? [])
+      // 需要把文件夹过滤掉 (文件夹没有 type)
+      const files = Array.from(e.clipboardData?.files ?? []).filter(
+        (f) => !!f.type
+      )
       if (files.length > 0) {
         upload(files)
       }
