d9a4677f3212ad7b9fe8df586fb6b508ff17e681
xiaomingTang <1038761793@qq.com>
2023-07-30 16:38:46 +0800

    feat: 恢复 header 中的入口

--- src/layout/DefaultHeader.tsx
+++ src/layout/DefaultHeader.tsx
@@ -4,6 +4,7 @@ import { UserButton } from './CornerButtons/UserButton'
 import { ThemeToggleButton } from './CornerButtons/ThemeToggleButton'
 import { BlogEntry } from './CornerButtons/Entries'
 import { RssButton } from './CornerButtons/RssButton'
+import { UploadTrigger } from './CornerButtons/UploadTrigger'
 
 import { dark, light } from '@/utils/theme'
 
@@ -58,6 +59,7 @@ export function DefaultRawHeader() {
             <BlogEntry />
           </Stack>
           <Stack direction='row'>
+            <UploadTrigger />
             <UserButton />
             <ThemeToggleButton />
             <RssButton />
