89d0160b98ae5fedd19d945854065fe5196e4950
tangqiulin <tangqiulin@box3.ink>
2023-08-10 16:24:24 +0800

    perf: 抽象了一个 IndexManager, 用于管理 index

--- src/hooks/useInjectHistory.ts
+++ src/hooks/useInjectHistory.ts
@@ -1,13 +1,17 @@
 import { useListen } from './useListen'
 
+import { IndexManager } from '@/utils/IndexManager'
+
 import { useEventCallback } from '@mui/material'
 import { useRef } from 'react'
 import { useEvent } from 'react-use'
 
 import type { NiceModalHandler } from '@ebay/nice-modal-react'
 
-let modalLevels: number[] = []
-let ignoreLevel = -1
+let ignoreIdx = -1
+// 这个 manager 既非 stack, 也非 queue,
+// 叫 stack 只是随便叫的
+const stack = new IndexManager()
 
 export function useInjectHistory(
   modal: NiceModalHandler<Record<string, unknown>>,
@@ -17,30 +21,42 @@ export function useInjectHistory(
    */
   onPopState: (e: PopStateEvent) => Promise<void>
 ) {
-  const levelRef = useRef(-1)
+  // 弹窗维护各自的 index
+  const modalIdxRef = useRef(-1)
+
   const finalOnPopState = useEventCallback((e: PopStateEvent) => {
-    if (levelRef.current !== Math.max(...modalLevels, 0)) {
+    // 轮到我了吗? 没轮到就返回
+    if (modalIdxRef.current !== stack.latest) {
       return
     }
-    if (levelRef.current === ignoreLevel) {
-      ignoreLevel = -1
+    // 是上一个弹窗关闭而触发的 popstate 吗? 是就返回;
+    // (并且把 ignoreIdx 标志位清掉, 使得下次 popstate 能正常生效)
+    if (modalIdxRef.current === ignoreIdx) {
+      ignoreIdx = -1
       return
     }
-    modalLevels = modalLevels.filter((v) => v !== levelRef.current)
+    stack.drop(modalIdxRef.current)
     onPopState(e).catch(() => {
-      modalLevels.push(levelRef.current)
+      // 抛错时, 恢复 stack
+      stack.push(modalIdxRef.current)
       window.history.pushState(null, '', window.location.href)
     })
   })
+
   useEvent('popstate', finalOnPopState)
+
   useListen(modal.visible, () => {
     if (modal.visible) {
-      levelRef.current = Math.max(...modalLevels, 0) + 1
-      modalLevels.push(levelRef.current)
+      // modal index 初始化
+      modalIdxRef.current = stack.latest + 1
+      stack.push(modalIdxRef.current)
       window.history.pushState(null, '', window.location.href)
-    } else if (modalLevels.includes(levelRef.current)) {
-      modalLevels = modalLevels.filter((v) => v !== levelRef.current)
-      ignoreLevel = levelRef.current - 1
+    } else if (stack.has(modalIdxRef.current)) {
+      // 站内有该 modal index, 说明是其他地方手动调用 modal.hide, 而非物理返回所触发
+      stack.drop(modalIdxRef.current)
+      // 此时需要先清理 stack, 然后根据新 stack 设置标志位
+      ignoreIdx = stack.latest
+      // 设置标志位是为了避免下一个 modal 被此处触发的 popstate 关闭
       window.history.back()
     }
   })
