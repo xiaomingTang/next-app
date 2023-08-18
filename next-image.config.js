a9820852c9f8ea4eda4f1af39e6c16bef3ff3224
xiaomingTang <1038761793@qq.com>
2023-08-19 01:30:41 +0800

    feat: s3 资源无需优化

--- next-image.config.js
+++ next-image.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   images: {
     minimumCacheTTL: 60 * 60,
-    domains: [
-      'tytcn.cn',
-      'poople.cc',
-      'usb250.com',
-      '12px.cc',
-      'next-app-storage-04a4aa9a124907-staging.s3.ap-northeast-2.amazonaws.com',
-    ],
+    domains: ['tytcn.cn', 'poople.cc', 'usb250.com', '12px.cc'],
   },
 }
