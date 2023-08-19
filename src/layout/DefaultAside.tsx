a1d65b5cd0869e3a04b3300c9118c1421976e4fd
xiaomingTang <1038761793@qq.com>
2023-08-20 03:14:35 +0800

    fix: again aside position move when scrollbar disappeared

--- src/layout/DefaultAside.tsx
+++ src/layout/DefaultAside.tsx
@@ -10,6 +10,7 @@ type DefaultAsideProps = BoxProps & {
 
 const ASIDE_WIDTH = 256
 const ASIDE_MARGIN = 0
+const SCROLL_BAR_WIDTH = 17
 
 export function DefaultAside({
   placement,
@@ -26,15 +27,29 @@ export function DefaultAside({
         position: 'fixed',
         width: `${ASIDE_WIDTH}px`,
         top: '56px',
+        /**
+         * 50% 基准是不含滚动条的;
+         * 50vw 基准是含滚动条的;
+         */
         left:
           placement === 'left'
-            ? `calc(50% - ${
-                theme.v.screens.desktop / 2 + ASIDE_WIDTH + ASIDE_MARGIN
+            ? `calc(50vw - ${
+                theme.v.screens.desktop / 2 +
+                ASIDE_WIDTH +
+                ASIDE_MARGIN +
+                SCROLL_BAR_WIDTH / 2
               }px)`
-            : `calc(50% + ${theme.v.screens.desktop / 2 + ASIDE_MARGIN}px)`,
+            : `calc(50vw + ${
+                theme.v.screens.desktop / 2 +
+                ASIDE_MARGIN -
+                SCROLL_BAR_WIDTH / 2
+              }px)`,
         [theme.breakpoints.down(
           // 17 是竖直滚动条的宽度
-          theme.v.screens.desktop + ASIDE_WIDTH * 2 + ASIDE_MARGIN * 2 + 17
+          theme.v.screens.desktop +
+            ASIDE_WIDTH * 2 +
+            ASIDE_MARGIN * 2 +
+            SCROLL_BAR_WIDTH
         )]: {
           display: 'none',
         },
