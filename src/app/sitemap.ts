fb29b16d2b009869898564312baa11b7dd4c20db
xiaomingTang <1038761793@qq.com>
2023-08-19 23:45:20 +0800

    feat: sitemap 新增 cards 页面

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { getBlogs } from './admin/blog/server'
 import { getTags } from './admin/tag/server'
+import { getMediaCards } from './cards/server'
 
 import { SA } from '@/errors/utils'
 import { resolvePath } from '@/utils/url'
@@ -7,6 +8,7 @@ import { resolvePath } from '@/utils/url'
 import { unstable_cache } from 'next/cache'
 
 import type { MetadataRoute } from 'next'
+import type { MediaCardType } from '@prisma/client'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogs = await unstable_cache(
@@ -16,7 +18,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       }),
     ['getBlogs', 'PUBLISHED'],
     {
-      revalidate: 300,
+      revalidate: 86400,
       tags: ['getBlogs'],
     }
   )().then(SA.decode)
@@ -24,22 +26,66 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogRoutes = blogs.map((blog) => ({
     url: resolvePath(`/blog/${blog.hash}`).href,
     lastModified: blog.updatedAt,
+    priority: 0.8,
   }))
 
   const tags = await unstable_cache(() => getTags({}), ['getTags'], {
-    revalidate: 300,
+    revalidate: 86400,
     tags: ['getTags'],
   })().then(SA.decode)
 
   const tagRoutes = tags.map((tag) => ({
     url: resolvePath(`/tag/${tag.hash}`).href,
     lastModified: tag.updatedAt,
+    priority: 0.6,
   }))
 
-  const otherRoutes = ['/'].map((route) => ({
-    url: resolvePath(route).href,
-    lastModified: new Date().toISOString(),
-  }))
+  const cards = await unstable_cache(
+    () => getMediaCards({}),
+    ['getMediaCards'],
+    {
+      revalidate: 86400,
+      tags: ['getMediaCards'],
+    }
+  )().then(SA.decode)
 
-  return [...blogRoutes, ...tagRoutes, ...otherRoutes]
+  const getCardsLastModifiedTime = (type: MediaCardType) =>
+    Math.max(
+      ...cards.filter((c) => c.type === type).map((c) => c.updatedAt.getTime())
+    )
+
+  return [
+    {
+      url: resolvePath('/').href,
+      lastModified: new Date(
+        Math.max(
+          ...blogs.map((b) => b.updatedAt.getTime()),
+          ...tags.map((b) => b.updatedAt.getTime())
+        )
+      ),
+      priority: 1,
+    },
+    ...blogRoutes,
+    ...tagRoutes,
+    {
+      url: resolvePath('/cards/area').href,
+      lastModified: new Date(getCardsLastModifiedTime('AREA')),
+      priority: 0.4,
+    },
+    {
+      url: resolvePath('/cards/colors').href,
+      lastModified: new Date(getCardsLastModifiedTime('COLOR')),
+      priority: 0.4,
+    },
+    {
+      url: resolvePath('/cards/foods').href,
+      lastModified: new Date(getCardsLastModifiedTime('FOOD')),
+      priority: 0.4,
+    },
+    {
+      url: resolvePath('/cards/fruits').href,
+      lastModified: new Date(getCardsLastModifiedTime('FRUIT')),
+      priority: 0.4,
+    },
+  ]
 }
