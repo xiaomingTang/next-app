584f231503e9ee79b57225826a9605bb450f000d
xiaomingTang <1038761793@qq.com>
2023-08-21 23:31:39 +0800

    ci: perf deploy: upload with ssh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# ftp required: sudo yum install ftp
 # ssh required: sudo yum install ssh
 # sshpass required: sudo yum install sshpass
 # $P1_xxx series env variables is required
@@ -27,31 +26,30 @@ zip_file_name=".prod-$(date +%Y-%m-%d-%H-%M-%S).zip"
 echo zip $zip_file_name ing...
 # -q: silent
 zip -r -q $zip_file_name .
+echo zip $zip_file_name finished
 
+echo upload $zip_file_name ing...
+sshpass -p $P1_SSH_PASSWORD scp -r $zip_file_name $P1_SSH_USER@$P1_SSH_HOST:/www/wwwroot/$P1_REMOTE_NAME
+echo upload $zip_file_name finished
 
-ftp -inv $P1_FTP_HOST <<EOF
-user $P1_FTP_USER $P1_FTP_PASSWORD
-mput $zip_file_name
-bye
-EOF
-
-# lsof -t -i:$port | xargs kill -15 # to be verified: not work
-# 之所以使用 `chown -R www .` 而非直接以 www 登录, 是 宝塔 www 用户不能用来登录...
+# lsof -t -i:$port | xargs kill -15
+# lsof -i:$port | grep LISTEN | awk '{print $2}' | xargs kill -15
 
 # pass them on the command line of the remote shell, and retrieve them via $1, $2: https://stackoverflow.com/a/37104048
 # upzip -o means replace files if exists and not ask
-sshpass -p $P1_SSH_PASSWORD ssh -p $P1_SSH_PORT -t $P1_SSH_USER@$P1_SSH_HOST "bash -s $P1_REMOTE_PORT $zip_file_name $P1_REMOTE_NAME" <<'EOL'
+sshpass -p $P1_SSH_PASSWORD ssh -t $P1_SSH_USER@$P1_SSH_HOST "bash -s $P1_REMOTE_PORT $zip_file_name $P1_REMOTE_NAME" <<'EOL'
   port=$1
   file_name=$2
   remote_name=$3
 
   cd /www/wwwroot/$remote_name
 
-  unzip -o $file_name
-  lsof -i:$port | grep LISTEN | awk '{print $2}' | xargs kill -15
-  bash /www/server/nodejs/vhost/scripts/$remote_name.sh
+  unzip -q -o $file_name
+  lsof -t -i:$port | xargs kill -15
+  nohup bash /www/server/nodejs/vhost/scripts/$remote_name.sh &>/dev/null
+  ls -at .prod-*.zip | sed -n '4,$p' | xargs -I {} rm -rf {}
   exit
-EOL
+EOL &
 
 git add .
 git stash
