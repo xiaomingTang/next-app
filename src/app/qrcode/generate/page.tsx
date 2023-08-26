5d28b8472707150aa450c570db42ed6712afb833
xiaomingTang <1038761793@qq.com>
2023-08-26 23:01:21 +0800

    feat: 不造轮子了, 用草料二维码

--- src/app/qrcode/generate/page.tsx
+++ src/app/qrcode/generate/page.tsx
@@ -1,15 +1,11 @@
-import DefaultLayout from '@/layout/DefaultLayout'
-import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
 import { seo } from '@/utils/seo'
 
+import { redirect } from 'next/navigation'
+
 export const metadata = seo.defaults({
-  title: '生成二维码 (施工中...)',
+  title: '生成二维码',
 })
 
 export default async function Home() {
-  return (
-    <DefaultLayout>
-      <DefaultBodyContainer>生成二维码 (施工中...)</DefaultBodyContainer>
-    </DefaultLayout>
-  )
+  redirect('https://cli.im/')
 }
