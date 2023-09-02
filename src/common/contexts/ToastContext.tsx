f6f3b0072a1fef0ed48c7c51d202359086ec26c6
xiaomingTang <1038761793@qq.com>
2023-09-02 21:46:50 +0800

    style: fix: icon flex 0

--- src/common/contexts/ToastContext.tsx
+++ src/common/contexts/ToastContext.tsx
@@ -19,16 +19,22 @@ export function ToastContext() {
           }),
         },
         success: {
-          icon: <SvgSuccess className='fill-primary-light text-[18px]' />,
+          icon: (
+            <SvgSuccess className='fill-primary-light text-[18px] flex-[0_0_auto]' />
+          ),
         },
         loading: {
           // 默认 Infinity, 但是这里设置为 4000ms
           // 需要 Infinity 时自己明确设置
           duration: 4000,
-          icon: <SvgLoading className='animate-spin text-[18px]' />,
+          icon: (
+            <SvgLoading className='animate-spin text-[18px] flex-[0_0_auto]' />
+          ),
         },
         error: {
-          icon: <SvgError className='fill-error-light text-[18px]' />,
+          icon: (
+            <SvgError className='fill-error-light text-[18px] flex-[0_0_auto]' />
+          ),
         },
       }}
     />
