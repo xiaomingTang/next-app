c11a5d8e90536bd4369104b3c663d0863992c22f
xiaomingTang <1038761793@qq.com>
2023-09-15 01:44:31 +0800

    feat: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -82,6 +82,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...blogRoutes,
     ...tagRoutes,
     {
+      url: resolvePath('/wallpaper').href,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
       url: resolvePath('/cards/area').href,
       lastModified: new Date(getCardsLastModifiedTime('AREA')),
       changeFrequency: 'monthly',
