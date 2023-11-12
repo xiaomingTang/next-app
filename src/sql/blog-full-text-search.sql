a796ccd314d3cc2408cc071ea9d06580f12e19a4
xiaomingTang <1038761793@qq.com>
2023-11-12 22:50:37 +0800

    feat: update db name

--- src/sql/blog-full-text-search.sql
+++ src/sql/blog-full-text-search.sql
@@ -1,25 +1,25 @@
 /**
  * 由于 prisma 暂不支持 NATURAL LANGUAGE MODE, 所以只能执行 raw query.
  * 该 sql 需要执行前置命令:
- * ALTER TABLE `tytcn-nest`.`Blog` ADD FULLTEXT `fulltext_title_description_content` (`title`, `description`, `content`) WITH PARSER NGRAM;
+ * ALTER TABLE `tytcn`.`Blog` ADD FULLTEXT `fulltext_title_description_content` (`title`, `description`, `content`) WITH PARSER NGRAM;
  */
 
 SELECT
-    `tytcn-nest`.`Blog`.`hash`,
-    `tytcn-nest`.`Blog`.`type`,
-    `tytcn-nest`.`Blog`.`createdAt`,
-    `tytcn-nest`.`Blog`.`updatedAt`,
-    `tytcn-nest`.`Blog`.`title`,
-    `tytcn-nest`.`Blog`.`content`,
-    `tytcn-nest`.`Blog`.`description`,
-    `tytcn-nest`.`Blog`.`creatorId`
-FROM `tytcn-nest`.`Blog`
+    `tytcn`.`Blog`.`hash`,
+    `tytcn`.`Blog`.`type`,
+    `tytcn`.`Blog`.`createdAt`,
+    `tytcn`.`Blog`.`updatedAt`,
+    `tytcn`.`Blog`.`title`,
+    `tytcn`.`Blog`.`content`,
+    `tytcn`.`Blog`.`description`,
+    `tytcn`.`Blog`.`creatorId`
+FROM `tytcn`.`Blog`
 WHERE (
-        `tytcn-nest`.`Blog`.`type` = "PUBLISHED"
+        `tytcn`.`Blog`.`type` = "PUBLISHED"
         AND MATCH (
-            `tytcn-nest`.`Blog`.`title`,
-            `tytcn-nest`.`Blog`.`description`,
-            `tytcn-nest`.`Blog`.`content`
+            `tytcn`.`Blog`.`title`,
+            `tytcn`.`Blog`.`description`,
+            `tytcn`.`Blog`.`content`
         ) AGAINST (? IN NATURAL LANGUAGE MODE)
     )
 LIMIT 5
