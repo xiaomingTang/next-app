6782f14c8652a671876d6e38e628f9661bf98803
xiaomingTang <1038761793@qq.com>
2023-11-14 00:05:03 +0800

    feat: Dialog default transition 改为 Fade

--- src/components/SlideUpTransition.tsx
+++ src/components/SlideUpTransition.tsx
@@ -2,6 +2,7 @@
 
 import { forwardRef } from 'react'
 import Slide from '@mui/material/Slide'
+import { Fade } from '@mui/material'
 
 import type { TransitionProps } from '@mui/material/transitions'
 
@@ -15,4 +16,16 @@ function RawSlideUpTransition(
 }
 
 export const SlideUpTransition = forwardRef(RawSlideUpTransition)
-export const DefaultDialogTransition = undefined
+
+function RawFadeTransition(
+  props: TransitionProps & {
+    children: React.ReactElement
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Fade ref={ref} {...props} />
+}
+
+export const FadeTransition = forwardRef(RawFadeTransition)
+
+export const DefaultDialogTransition = FadeTransition
