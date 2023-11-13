69b5b34e3f59cb2e6f6956199c5ab0618dfd47a8
xiaomingTang <1038761793@qq.com>
2023-11-14 00:18:48 +0800

    feat: CustomLink 新增 forwardRef

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,11 +1,17 @@
 import NextLink from 'next/link'
+import { forwardRef } from 'react'
 
 import type { LinkProps } from 'next/link'
 
-export function Link({
-  ...props
-}: LinkProps & {
-  children?: React.ReactNode | React.ReactNode[]
-}) {
-  return <NextLink {...props} />
+function RawLink(
+  {
+    ...props
+  }: LinkProps & {
+    children?: React.ReactNode | React.ReactNode[]
+  },
+  ref: React.ForwardedRef<HTMLAnchorElement>
+) {
+  return <NextLink ref={ref} {...props} />
 }
+
+export const Link = forwardRef(RawLink)
