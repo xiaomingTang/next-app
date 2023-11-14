be22c318e2a702db7a5c0f2eb7beb092167bc473
xiaomingTang <1038761793@qq.com>
2023-11-14 21:25:45 +0800

    feat: 新增 youtube 和 bilibili url 的格式化

--- src/app/(default-layout)/blog/components/markdownComponents/index.tsx
+++ src/app/(default-layout)/blog/components/markdownComponents/index.tsx
@@ -90,10 +90,34 @@ function Video({ src = '', poster = '' }: { src?: string; poster?: string }) {
   )
 }
 
-function Iframe({ src = '' }: { src?: string }) {
+function formatIframeSrc(src?: string) {
   if (!src) {
-    return <></>
+    return null
   }
+  const srcUrl = new URL(src, ENV_CONFIG.public.origin)
+  if (srcUrl.host === 'bilibili.com' || srcUrl.host.endsWith('.bilibili.com')) {
+    srcUrl.searchParams.set('autoplay', '0')
+    return srcUrl
+  }
+  /**
+   * youtube 嵌入网址必须为:
+   * https://www.youtube.com/embed/yU-GOqtFei0
+   */
+  if (srcUrl.host === 'youtube.com' || srcUrl.host.endsWith('.youtube.com')) {
+    // https://www.youtube.com/watch?v=yU-GOqtFei0
+    if (srcUrl.pathname === '/watch' && srcUrl.searchParams.get('v')) {
+      srcUrl.pathname = `/embed/${srcUrl.searchParams.get('v')}`
+      return srcUrl
+    }
+  }
+  // https://youtu.be/yU-GOqtFei0
+  if (srcUrl.host === 'youtu.be' || srcUrl.host.endsWith('.youtu.be')) {
+    srcUrl.host = 'youtube.com'
+    srcUrl.pathname = `/embed${srcUrl.pathname}`
+    return srcUrl
+  }
+
+  // 以下是所有白名单
   const whiteListPatterns: RemotePattern[] = [
     { hostname: 'youtu.be' },
     { hostname: 'youtube.com' },
@@ -102,27 +126,35 @@ function Iframe({ src = '' }: { src?: string }) {
     { hostname: '**.youtube.com' },
     { hostname: '**.bilibili.com' },
   ]
-  const srcUrl = new URL(src, ENV_CONFIG.public.origin)
 
   if (
     whiteListPatterns.some((pattern) => matchRemotePattern(pattern, srcUrl))
   ) {
-    return (
-      <iframe
-        src={src}
-        width={720}
-        height={450}
-        allowFullScreen
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-        style={{
-          width: '100%',
-          maxWidth: 720,
-        }}
-      />
-    )
+    return srcUrl
+  }
+  return null
+}
+
+function Iframe({ src = '' }: { src?: string }) {
+  const srcUrl = formatIframeSrc(src)
+
+  if (!srcUrl) {
+    return <></>
   }
 
-  return <></>
+  return (
+    <iframe
+      src={srcUrl.href}
+      width={720}
+      height={450}
+      allowFullScreen
+      allow='accelerometer; encrypted-media; gyroscope; picture-in-picture; web-share'
+      style={{
+        width: '100%',
+        maxWidth: 720,
+      }}
+    />
+  )
 }
 
 export const markdownComponents: MDXComponents = {
