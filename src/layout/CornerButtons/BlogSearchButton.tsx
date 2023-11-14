091c88a1ecb28d3cf637fe31dbaf43533ba7c578
xiaomingTang <1038761793@qq.com>
2023-11-14 20:36:39 +0800

    feat: 新增搜索快捷键

--- src/layout/CornerButtons/BlogSearchButton.tsx
+++ src/layout/CornerButtons/BlogSearchButton.tsx
@@ -5,14 +5,22 @@ import { BlogSearchSection } from './BlogSearchSection'
 import { IconButton } from '@mui/material'
 import NiceModal from '@ebay/nice-modal-react'
 import SearchIcon from '@mui/icons-material/Search'
+import { useKeyPressEvent } from 'react-use'
 
-// TODO: 博客搜索
 export function BlogSearchButton() {
+  useKeyPressEvent(
+    (e) => e.ctrlKey && e.key.toLowerCase() === 'f',
+    (e) => {
+      e.preventDefault()
+      NiceModal.show(BlogSearchSection)
+    }
+  )
   return (
     <>
       <IconButton
         className='text-primary-main'
-        aria-label='搜索博客标题和内容 (仅支持英文搜索)'
+        aria-label='搜索博客标题和内容 (ctrl + F)'
+        title='搜索博客标题和内容 (ctrl + F)'
         onClick={() => {
           NiceModal.show(BlogSearchSection)
         }}
