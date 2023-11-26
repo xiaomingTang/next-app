580f0c4d76cbbc57c8446ce8beaeb277874e0e44
xiaomingTang <1038761793@qq.com>
2023-11-26 22:19:53 +0800

    feat: mp3 添加 controls 方法

--- src/components/GlobalAudioPlayer.tsx
+++ src/components/GlobalAudioPlayer.tsx
@@ -5,13 +5,21 @@ import { useListen } from '@/hooks/useListen'
 
 import { create } from 'zustand'
 import { useAudio as useReactUseAudio } from 'react-use'
+import { useMemo } from 'react'
+
+import type { CustomMP3 } from '@prisma/client'
 
 type UseAudioRet = ReturnType<typeof useReactUseAudio>
 
 export const useAudio = create<{
+  activeMp3?: CustomMP3
   audio: UseAudioRet[0]
   state: UseAudioRet[1]
-  controls: UseAudioRet[2]
+  controls: UseAudioRet[2] & {
+    switch: (mp3: CustomMP3) => void
+    next: () => void
+    prev: () => void
+  }
   ref?: UseAudioRet[3]
   loading: boolean
 }>(() => ({
@@ -32,18 +40,24 @@ export const useAudio = create<{
     volume: () => undefined,
     mute: () => undefined,
     unmute: () => undefined,
+    switch: () => undefined,
+    next: () => undefined,
+    prev: () => undefined,
   },
   ref: undefined,
   loading: false,
 }))
 
-const props: Parameters<typeof useReactUseAudio>[0] = {
-  src: '',
-  loop: true,
-}
-
 export function GlobalAudioPlayer() {
   const { loading, setMedia } = useMediaLoading()
+  const activeMp3 = useAudio((state) => state.activeMp3)
+  const props: Parameters<typeof useReactUseAudio>[0] = useMemo(
+    () => ({
+      src: activeMp3?.mp3 ?? '',
+      loop: true,
+    }),
+    [activeMp3]
+  )
 
   const [audio, state, controls, ref] = useReactUseAudio(props)
 
@@ -54,7 +68,40 @@ export function GlobalAudioPlayer() {
     useAudio.setState({ state })
   })
   useListen(controls, () => {
-    useAudio.setState({ controls })
+    useAudio.setState({
+      controls: {
+        ...controls,
+        switch: (mp3) => {
+          useAudio.setState({
+            activeMp3: mp3,
+          })
+        },
+        next: () => {
+          const { mp3s } = globalThis
+          if (mp3s.length === 0) {
+            return
+          }
+          const activeIndex = mp3s.findIndex(
+            (item) => item.hash === activeMp3?.hash
+          )
+          useAudio.setState({
+            activeMp3: mp3s[(activeIndex + 1 + mp3s.length) % mp3s.length],
+          })
+        },
+        prev: () => {
+          const { mp3s } = globalThis
+          if (mp3s.length === 0) {
+            return
+          }
+          const activeIndex = mp3s.findIndex(
+            (item) => item.hash === activeMp3?.hash
+          )
+          useAudio.setState({
+            activeMp3: mp3s[(activeIndex - 1 + mp3s.length) % mp3s.length],
+          })
+        },
+      },
+    })
   })
   useListen(ref, () => {
     useAudio.setState({ ref })
