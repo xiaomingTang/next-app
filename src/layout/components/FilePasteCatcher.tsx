b5b747f7190e0f44509dcc2b8d7cc3b230ad771c
xiaomingTang <1038761793@qq.com>
2023-11-30 21:14:35 +0800

    feat: 新增拖拽上传文件

--- src/layout/components/FilePasteCatcher.tsx
+++ src/layout/components/FilePasteCatcher.tsx
@@ -2,12 +2,18 @@
 
 import { upload } from '@D/upload/components/Uploader'
 import { useUser } from '@/user'
+import { dark } from '@/utils/theme'
 
 import { noop } from 'lodash-es'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Box, Fade, Typography, alpha } from '@mui/material'
+import { common } from '@mui/material/colors'
 
 export function FilePasteCatcher() {
   const user = useUser()
+  const [dragging, setDragging] = useState(false)
+
+  // 监听文件粘贴
   useEffect(() => {
     if (!user.id) {
       return noop
@@ -27,5 +33,84 @@ export function FilePasteCatcher() {
     }
   }, [user.id])
 
-  return <></>
+  // 监听文件拖拽
+  useEffect(() => {
+    const dndElement = document.documentElement
+    const dragHandler = (e: DragEvent, cb: (files: FileList) => void) => {
+      const files = e.dataTransfer?.files
+      if (files) {
+        e.preventDefault()
+        cb(files)
+      }
+    }
+    const onDragenter = (e: DragEvent) => {
+      dragHandler(e, () => {
+        setDragging(true)
+      })
+    }
+    const onDragover = (e: DragEvent) => {
+      dragHandler(e, () => {
+        setDragging(true)
+      })
+    }
+    const onDragleave = (e: DragEvent) => {
+      dragHandler(e, () => {
+        setDragging(false)
+      })
+    }
+    const onDrop = (e: DragEvent) => {
+      dragHandler(e, (files) => {
+        setDragging(false)
+        upload([...files])
+      })
+    }
+
+    dndElement.addEventListener('dragenter', onDragenter)
+    dndElement.addEventListener('dragover', onDragover)
+    dndElement.addEventListener('dragleave', onDragleave)
+    dndElement.addEventListener('drop', onDrop)
+
+    return () => {
+      dndElement.addEventListener('dragenter', onDragenter)
+      dndElement.addEventListener('dragover', onDragover)
+      dndElement.addEventListener('dragleave', onDragleave)
+      dndElement.addEventListener('drop', onDrop)
+    }
+  }, [])
+
+  return (
+    <Fade in={dragging}>
+      <Box
+        sx={{
+          zIndex: (theme) => theme.zIndex.tooltip + 1000,
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          right: 0,
+          bottom: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '24px',
+          fontWeight: 'bold',
+          userSelect: 'none',
+          backdropFilter: 'blur(8px)',
+          backgroundColor: alpha(common.black, 0.8),
+          color: 'white',
+          [dark()]: {
+            backgroundColor: alpha(common.white, 0.5),
+            color: 'black',
+          },
+        }}
+      >
+        <Typography
+          sx={{
+            letterSpacing: '4px',
+          }}
+        >
+          预备上传
+        </Typography>
+      </Box>
+    </Fade>
+  )
 }
