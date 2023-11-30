2bbcbffd2922d050ec98d772678c4fb6b262c728
xiaomingTang <1038761793@qq.com>
2023-11-30 20:31:23 +0800

    fix: 部分文件粘贴上传无效

--- src/layout/components/FilePasteCatcher.tsx
+++ src/layout/components/FilePasteCatcher.tsx
@@ -15,7 +15,7 @@ export function FilePasteCatcher() {
     const onPaste = (e: ClipboardEvent) => {
       // 需要把文件夹过滤掉 (文件夹没有 type)
       const files = Array.from(e.clipboardData?.files ?? []).filter(
-        (f) => !!f.type
+        (f) => f.size > 0
       )
       if (files.length > 0) {
         upload(files)
