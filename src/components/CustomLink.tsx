f8688316612087746a1cdc840024d2ee46b9eb19
xiaomingTang <1038761793@qq.com>
2023-12-08 00:00:01 +0800

    feat: export LinkWithReplace

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,17 +1,27 @@
+'use client'
+
 import NextLink from 'next/link'
 import { forwardRef } from 'react'
 
 import type { LinkProps } from 'next/link'
 
-function RawLink(
-  {
-    ...props
-  }: LinkProps & {
-    children?: React.ReactNode | React.ReactNode[]
-  },
-  ref: React.ForwardedRef<HTMLAnchorElement>
-) {
-  return <NextLink ref={ref} {...props} />
+export function geneLink(defaultProps?: Partial<LinkProps>) {
+  function RawLink(
+    {
+      ...props
+    }: LinkProps & {
+      children?: React.ReactNode | React.ReactNode[]
+    },
+    ref: React.ForwardedRef<HTMLAnchorElement>
+  ) {
+    return <NextLink ref={ref} {...defaultProps} {...props} />
+  }
+
+  return forwardRef(RawLink)
 }
 
-export const Link = forwardRef(RawLink)
+export const Link = geneLink()
+
+export const LinkWithReplace = geneLink({
+  replace: true,
+})
