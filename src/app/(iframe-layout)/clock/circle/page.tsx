fa9a421d4835e368e79db2bdc9ddc1e5b8cf5edf
tangqiulin <tangqiulin@box3.ink>
2023-12-12 20:38:11 +0800

    feat: 引入 Clock svg

--- src/app/(iframe-layout)/clock/circle/page.tsx
+++ src/app/(iframe-layout)/clock/circle/page.tsx
@@ -1,11 +1,5 @@
 import { CircleClock } from '../components/CircleClock'
 
-import { NoSsr } from '@mui/material'
-
 export default function Index() {
-  return (
-    <NoSsr>
-      <CircleClock />
-    </NoSsr>
-  )
+  return <CircleClock />
 }
--- src/app/(iframe-layout)/clock/components/CircleClock.tsx
+++ src/app/(iframe-layout)/clock/components/CircleClock.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import Clock from './svgs/时钟-万花筒-皇冠.svg?icon'
+
 import { useNowDate } from '@/hooks/useNow'
 
-import { Box } from '@mui/material'
+import { Box, NoSsr } from '@mui/material'
 
-export function CircleClock() {
+export function RawCircleClock() {
   const now = useNowDate()
   const h = now.getHours()
   const m = now.getMinutes()
@@ -14,6 +16,15 @@ export function CircleClock() {
     <Box>
       {h.toString().padStart(2, '0')}:{m.toString().padStart(2, '0')}:
       {s.toString().padStart(2, '0')}
+      <Clock width={300} height={300} />
     </Box>
+  )
+}
+
+export function CircleClock() {
+  return (
+    <NoSsr>
+      <RawCircleClock />
+    </NoSsr>
   )
 }
