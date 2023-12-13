8091203eeafe4b491a3173fce55a66f204078ba9
xiaomingTang <1038761793@qq.com>
2023-12-14 00:17:47 +0800

    perf: useHover use pointer events

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,38 +1,26 @@
 import { noop } from 'lodash-es'
-import { useState, type MutableRefObject, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
-export function useHover(
-  ref: MutableRefObject<unknown> | null | undefined,
-  enabled = true
-) {
+export function useHover() {
   const [hovered, setHovered] = useState(false)
+  const [elem, setElem] = useState<HTMLElement | undefined | null>()
 
   useEffect(() => {
-    if (!enabled) {
+    if (!elem) {
       return noop
     }
-    const onMouseOver = () => setHovered(true)
-    const onMouseOut = () => setHovered(false)
-    const elem = ref?.current as HTMLElement | null
-    if (elem) {
-      elem.addEventListener('touchstart', onMouseOver)
-      elem.addEventListener('mouseover', onMouseOver)
+    const onEnter = () => setHovered(true)
+    const onLeave = () => setHovered(false)
+    elem.addEventListener('pointerenter', onEnter)
+    elem.addEventListener('pointerleave', onLeave)
+    window.addEventListener('pointercancel', onLeave)
 
-      elem.addEventListener('mouseout', onMouseOut)
-      window.addEventListener('touchend', onMouseOut)
-      window.addEventListener('touchcancel', onMouseOut)
-    }
     return () => {
-      if (elem) {
-        elem.removeEventListener('touchstart', onMouseOver)
-        elem.removeEventListener('mouseover', onMouseOver)
-
-        elem.removeEventListener('mouseout', onMouseOut)
-        window.removeEventListener('touchend', onMouseOut)
-        window.removeEventListener('touchcancel', onMouseOut)
-      }
+      elem.removeEventListener('pointerenter', onEnter)
+      elem.removeEventListener('pointerleave', onLeave)
+      window.removeEventListener('pointercancel', onLeave)
     }
-  }, [enabled, ref])
+  }, [elem])
 
-  return hovered
+  return [hovered, setElem] as const
 }
