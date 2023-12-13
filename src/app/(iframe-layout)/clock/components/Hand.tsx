df820b0615d6363caadccd295f61a41bfacf6e27
tangqiulin <tangqiulin@box3.ink>
2023-12-13 19:13:25 +0800

    feat: 指针添加动画

--- src/app/(iframe-layout)/clock/components/Hand.tsx
+++ src/app/(iframe-layout)/clock/components/Hand.tsx
@@ -1,10 +1,12 @@
+import { remainder } from '@/utils/math'
+
 import { Box } from '@mui/material'
 
 export function Hand({
   center,
   width,
   height,
-  degree,
+  degree: inputDegree,
   color,
 }: {
   center: [number, number]
@@ -14,6 +16,7 @@ export function Hand({
   color: string
 }) {
   const [x, y] = center
+  const degree = remainder(inputDegree, 360)
   return (
     <Box
       component='rect'
@@ -23,7 +26,12 @@ export function Hand({
         rx: 9999,
         ry: 9999,
         fill: color,
+        transition: degree >= 354 || degree <= 6 ? 'none' : 'transform .25s',
+        // easeOutQuart
+        transitionTimingFunction: 'cubic-bezier(0.165, 0.84, 0.44, 1)',
         transformOrigin: `${x}px ${y}px`,
+      }}
+      style={{
         transform: `rotate(${degree}deg) translate(${x}px,${y - height / 2}px)`,
       }}
     />
