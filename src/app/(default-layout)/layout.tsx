2052d3dbf80906b72e3c1fb0abb34f44ec44a11a
xiaomingTang <1038761793@qq.com>
2023-12-14 01:30:11 +0800

    feat: default layout 新增左侧时钟挂件

--- src/app/(default-layout)/layout.tsx
+++ src/app/(default-layout)/layout.tsx
@@ -1,6 +1,11 @@
+import { HoverableClock } from './clock/HoverableClock'
+
 import { GA } from '@/analytics/GA'
+import { DefaultAside } from '@/layout/DefaultAside'
 import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
 import DefaultLayout from '@/layout/DefaultLayout'
+import { Clock } from '@I/clock/components/Clock'
+import { Dial, Hands } from '@I/clock/theme/13/exports'
 
 export default function DefaultLayoutTsx({
   children,
@@ -11,6 +16,14 @@ export default function DefaultLayoutTsx({
     <DefaultLayout>
       <DefaultBodyContainer>
         <GA />
+        <DefaultAside placement='left'>
+          <HoverableClock clockIframePath='/clock/theme/13'>
+            <Clock>
+              <Dial width='100%' height='100%' />
+              <Hands />
+            </Clock>
+          </HoverableClock>
+        </DefaultAside>
         {children}
       </DefaultBodyContainer>
     </DefaultLayout>
