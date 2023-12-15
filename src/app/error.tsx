4e3279a1e17bffae235c4b2660b8898e90ec2369
xiaomingTang <1038761793@qq.com>
2023-12-16 00:43:09 +0800

    feat: rm unused html

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,14 +3,7 @@
 // Error components must be Client Components
 
 import { GA } from '@/analytics/GA'
-import Contexts from '@/common/contexts'
-import Polyfills from '@/common/polyfills'
-import Providers from '@/common/providers'
 import { AlertError } from '@/components/Error'
-import { GetInitColorSchemeScript } from '@/components/GetColorScheme'
-import { GlobalAudioPlayer } from '@/components/GlobalAudioPlayer'
-import { LyricsViewer } from '@/components/LyricsViewer'
-import { VConsole } from '@/components/VConsole'
 import { toPlainError } from '@/errors/utils'
 import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
 import DefaultLayout from '@/layout/DefaultLayout'
@@ -31,23 +24,11 @@ export default function ErrorPage({
   }, [rawError])
 
   return (
-    <html lang='zh-cn' suppressHydrationWarning>
-      <body>
-        <VConsole />
-        <GetInitColorSchemeScript />
-        <Providers>
-          <GlobalAudioPlayer />
-          <LyricsViewer />
-          <Polyfills />
-          <Contexts />
-          <DefaultLayout>
-            <DefaultBodyContainer>
-              <GA />
-              <AlertError {...error} />
-            </DefaultBodyContainer>
-          </DefaultLayout>
-        </Providers>
-      </body>
-    </html>
+    <DefaultLayout>
+      <DefaultBodyContainer>
+        <GA />
+        <AlertError {...error} />
+      </DefaultBodyContainer>
+    </DefaultLayout>
   )
 }
