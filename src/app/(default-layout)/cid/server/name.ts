03751f9285b96eddabf70b934e6b3ac68af5b2c8
xiaomingTang <1038761793@qq.com>
2023-12-17 20:10:09 +0800

    feat: rename

--- src/app/(default-layout)/cid/server/name.ts
+++ src/app/(default-layout)/cid/server/name.ts
@@ -31,45 +31,47 @@ const NAME_MAP = {
   female: [girl1, girl2],
 }
 
-export const geneRandomName = SA.encode(async (props?: GetRandomNameProps) => {
-  const result: string[] = []
-  // 规范可能存在的用户输入
-  const count = clamp(props?.count ?? 10, 1, 100)
-  let lastNameLength = clamp(props?.lastNameLength ?? 1, 1, 2)
-  let gender = props?.gender ?? 'male'
-  if (!['male', 'female'].includes(gender)) {
-    gender = 'male'
-  }
+export const generateRandomName = SA.encode(
+  async (props?: GetRandomNameProps) => {
+    const result: string[] = []
+    // 规范可能存在的用户输入
+    const count = clamp(props?.count ?? 10, 1, 100)
+    let lastNameLength = clamp(props?.lastNameLength ?? 1, 1, 2)
+    let gender = props?.gender ?? 'male'
+    if (!['male', 'female'].includes(gender)) {
+      gender = 'male'
+    }
 
-  for (let i = 0; i < count; i += 1) {
-    const firstName = props?.firstName || weightedRandom(firstNameList)
-    // 如果用户未输入名字的位数，就使用随机值
-    if (typeof props?.lastNameLength !== 'number') {
-      lastNameLength = rand([1, 2])
-    }
-    // 如果用户未输入性别，就使用随机值
-    if (!props?.gender) {
-      gender = rand(['male', 'female'])
-    }
-    let fullName = ''
-    switch (lastNameLength) {
-      case 1: {
-        const [nameList] = NAME_MAP[gender]
-        fullName = `${firstName}${rand(nameList)}`
-        break
+    for (let i = 0; i < count; i += 1) {
+      const firstName = props?.firstName || weightedRandom(firstNameList)
+      // 如果用户未输入名字的位数，就使用随机值
+      if (typeof props?.lastNameLength !== 'number') {
+        lastNameLength = rand([1, 2])
       }
-      default: {
-        const [nameList1, nameList2] = NAME_MAP[gender]
-        fullName = `${firstName}${rand(nameList1)}${rand(nameList2)}`
-        break
+      // 如果用户未输入性别，就使用随机值
+      if (!props?.gender) {
+        gender = rand(['male', 'female'])
+      }
+      let fullName = ''
+      switch (lastNameLength) {
+        case 1: {
+          const [nameList] = NAME_MAP[gender]
+          fullName = `${firstName}${rand(nameList)}`
+          break
+        }
+        default: {
+          const [nameList1, nameList2] = NAME_MAP[gender]
+          fullName = `${firstName}${rand(nameList1)}${rand(nameList2)}`
+          break
+        }
+      }
+      if (result.includes(fullName)) {
+        i -= 1
+      } else {
+        result.push(fullName)
       }
     }
-    if (result.includes(fullName)) {
-      i -= 1
-    } else {
-      result.push(fullName)
-    }
-  }
 
-  return result
-})
+    return result
+  }
+)
