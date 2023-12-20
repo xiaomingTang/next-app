85a6c07ebdb230bc83490c52ba86ac2c4ab8f99b
xiaomingTang <1038761793@qq.com>
2023-12-20 20:36:08 +0800

    feat: 壁纸新增 seo

--- src/app/(fullscreen-layout)/wallpaper/page.tsx
+++ src/app/(fullscreen-layout)/wallpaper/page.tsx
@@ -4,6 +4,8 @@ import { seo } from '@/utils/seo'
 
 export const metadata = seo.defaults({
   title: '渐变色壁纸',
+  description: '自定义色彩渐变，自定义尺寸',
+  keywords: '壁纸,渐变,自定义',
 })
 
 export default async function Home() {
