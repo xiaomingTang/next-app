fd52c09bebbd2def04472c38e2fbbbf933f09780
xiaomingTang <1038761793@qq.com>
2023-12-30 16:33:40 +0800

    feat: 新增水平仪入口及 sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -105,6 +105,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.6,
     },
     {
+      url: resolvePath('/level').href,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
       url: resolvePath('/gotcha').href,
       lastModified: new Date(),
       changeFrequency: 'yearly',
--- src/layout/CornerButtons/constants.ts
+++ src/layout/CornerButtons/constants.ts
@@ -12,8 +12,9 @@ export const cardList: PathItem[] = [
 
 export const toolList: PathItem[] = [
   { pathname: '/clock', title: '时钟橱窗' },
-  { pathname: '/wallpaper', title: '壁纸' },
+  { pathname: '/level', title: '在线水平仪' },
   { pathname: '/cid', title: '随机生成身份证' },
+  { pathname: '/wallpaper', title: '壁纸' },
   { pathname: '/color', title: '色一色' },
   { pathname: '/gotcha', title: '试一试' },
 ]
