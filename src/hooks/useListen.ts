65523181f9e86a8a66eb918e58a52c18642d9241
xiaomingTang <1038761793@qq.com>
2024-01-11 11:51:34 +0800

    perf: useListen 首次 value 也要触发 callback

--- src/hooks/useListen.ts
+++ src/hooks/useListen.ts
@@ -1,3 +1,4 @@
+import { useEventCallback } from '@mui/material'
 import useEnhancedEffect from '@mui/material/utils/useEnhancedEffect'
 import { useRef } from 'react'
 
@@ -18,19 +19,16 @@ export function useListen<T>(
   value: T,
   callback: (next: T, prev: T | undefined) => void
 ) {
-  const prevRef = useRef(value)
-  const callbackRef = useRef(callback)
-
-  useEnhancedEffect(() => {
-    callbackRef.current = callback
-  })
+  const isFirstCallbackRef = useRef(true)
+  const prevRef = useRef<T | undefined>(undefined)
+  const callbackRef = useEventCallback(callback)
 
   useEnhancedEffect(() => {
     // useEffect 在 dev 环境会执行 2 遍, 此处避免该行为造成的影响
-    if (value === prevRef.current) {
+    if (value === prevRef.current && !isFirstCallbackRef.current) {
       return
     }
-    callbackRef.current(value, prevRef.current)
+    callbackRef(value, prevRef.current)
     prevRef.current = value
-  }, [value])
+  }, [value, callbackRef])
 }
