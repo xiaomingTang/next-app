92ca0035e10e4e060d29a0f19aa730564f949246
xiaomingTang <1038761793@qq.com>
2024-01-16 23:43:10 +0800

    feat: 简化 & 放宽 withStatic 类型

--- src/utils/withStatic.ts
+++ src/utils/withStatic.ts
@@ -1,26 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ENV_CONFIG } from '@/config'
 
-import type { StoreApi, UseBoundStore } from 'zustand'
-
 interface StaticFuncs {
   [key: string]: (...args: any[]) => any
 }
 
-type WithStatic<
-  T extends UseBoundStore<StoreApi<any>>,
-  S extends StaticFuncs
-> = T & S
-
-export function withStatic<T, S extends StaticFuncs>(
-  useStore: UseBoundStore<StoreApi<T>>,
+export function withStatic<T extends object, S extends StaticFuncs>(
+  useStore: T,
   staticFuncs: S
 ) {
-  const protectedKeys = Object.keys(useStore)
   const isDev = ENV_CONFIG.public.nodeEnv !== 'production'
-  const result = useStore as WithStatic<UseBoundStore<StoreApi<T>>, S>
+  const result = useStore as T & S
   Object.keys(staticFuncs).forEach((key) => {
-    if (protectedKeys.includes(key)) {
+    if (key in useStore) {
       if (isDev) {
         throw new Error(`protected key: ${key}`)
       } else {
@@ -28,7 +20,8 @@ export function withStatic<T, S extends StaticFuncs>(
       }
       return
     }
-    result[key as 'setState'] = staticFuncs[key]
+    // @ts-expect-error 将 staticFuncs key 赋给 result
+    result[key] = staticFuncs[key]
   })
   return result
 }
