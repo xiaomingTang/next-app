8619ea9e4a519ef09341b5c1cfb9e410c02bc1f4
tangqiulin <tangqiulin@box3.ink>
2024-03-07 18:12:06 +0800

    feat: 提取出 MessageWrapper

---
+++ src/app/(default-layout)/peer/components/MessageViewer/MessageWrapper.tsx
@@ -0,0 +1,101 @@
+import { dark } from '@/utils/theme'
+import { formatTime } from '@/utils/formatTime'
+
+import { Box, alpha } from '@mui/material'
+import { common } from '@mui/material/colors'
+
+import type { BoxProps } from '@mui/material'
+import type { MessageIns } from '../../type'
+
+const clearBoth = (
+  <Box
+    sx={{
+      width: '100%',
+      height: 0,
+      clear: 'both',
+      visibility: 'hidden',
+    }}
+  />
+)
+
+function MessageWrapper({ sx, children, ...restProps }: BoxProps) {
+  return (
+    <Box sx={{ py: 1, ...sx }} {...restProps}>
+      {children}
+    </Box>
+  )
+}
+
+export function MessageWrapperWithRole({
+  children,
+  role,
+  message,
+  ...restProps
+}: BoxProps & {
+  role: 'master' | 'guest'
+  message: Pick<MessageIns, 'date'>
+}) {
+  if (role === 'guest') {
+    return (
+      <MessageWrapper {...restProps}>
+        <Box
+          sx={{
+            fontSize: '11px',
+            ml: '4px',
+            mb: 1,
+            color: alpha(common.black, 0.6),
+            [dark()]: {
+              color: alpha(common.white, 0.5),
+            },
+          }}
+        >
+          {formatTime(message.date)}
+        </Box>
+        <Box
+          sx={{
+            display: 'inline-block',
+            minWidth: '2em',
+            maxWidth: '90%',
+            whiteSpace: 'pre-wrap',
+            wordBreak: 'break-all',
+          }}
+        >
+          {children}
+        </Box>
+        {clearBoth}
+      </MessageWrapper>
+    )
+  }
+  return (
+    <MessageWrapper {...restProps}>
+      <Box
+        sx={{
+          float: 'right',
+          fontSize: '11px',
+          mr: '4px',
+          mb: 1,
+          color: alpha(common.black, 0.6),
+          [dark()]: {
+            color: alpha(common.white, 0.5),
+          },
+        }}
+      >
+        {formatTime(message.date)}
+      </Box>
+      {clearBoth}
+      <Box
+        sx={{
+          float: 'right',
+          display: 'inline-block',
+          minWidth: '2em',
+          maxWidth: '90%',
+          whiteSpace: 'pre-wrap',
+          wordBreak: 'break-all',
+        }}
+      >
+        {children}
+      </Box>
+      {clearBoth}
+    </MessageWrapper>
+  )
+}
--- src/app/(default-layout)/peer/components/MessageViewer/index.tsx
+++ src/app/(default-layout)/peer/components/MessageViewer/index.tsx
@@ -1,122 +1,14 @@
+import { TextMessageElem } from './TextMessage'
+
 import { usePeerMessage } from '../../store/useMessage'
 import { usePeer } from '../../store/usePeer'
 
 import { dark } from '@/utils/theme'
-import { formatTime } from '@/utils/formatTime'
 import { useListen } from '@/hooks/useListen'
 
-import { Box, Typography, alpha } from '@mui/material'
-import { common, blue } from '@mui/material/colors'
+import { Box, alpha } from '@mui/material'
+import { common } from '@mui/material/colors'
 import { useRef } from 'react'
-
-import type { TextMessageIns } from '../../type'
-
-const clearBoth = (
-  <Box
-    sx={{
-      width: '100%',
-      height: 0,
-      clear: 'both',
-      visibility: 'hidden',
-    }}
-  />
-)
-
-function Text({ value: text, date, src }: TextMessageIns) {
-  const { peerId } = usePeer()
-  const role = src === peerId ? 'master' : 'guest'
-  if (role === 'guest') {
-    return (
-      <Box sx={{ py: 1 }}>
-        <Typography
-          sx={{
-            fontSize: '11px',
-            ml: '4px',
-            mb: 1,
-            color: alpha(common.black, 0.6),
-            [dark()]: {
-              color: alpha(common.white, 0.5),
-            },
-          }}
-        >
-          {formatTime(date)}
-        </Typography>
-        <Typography
-          sx={{
-            display: 'inline-block',
-            minWidth: '2em',
-            maxWidth: '90%',
-            p: 1,
-            borderRadius: 1,
-            whiteSpace: 'pre-wrap',
-            wordBreak: 'break-all',
-            border: '1px solid',
-            boxShadow: `0 0 8px ${alpha(common.black, 0.1)}`,
-            borderColor: alpha(common.black, 0.1),
-            backgroundColor: alpha(common.white, 0.75),
-            [dark()]: {
-              boxShadow: `0 0 8px ${alpha(common.white, 0.05)}`,
-              borderColor: alpha(common.white, 0.05),
-              backgroundColor: alpha(common.black, 0.25),
-            },
-          }}
-        >
-          {text}
-        </Typography>
-      </Box>
-    )
-  }
-
-  return (
-    <Box
-      sx={{
-        py: 1,
-      }}
-    >
-      <Typography
-        sx={{
-          float: 'right',
-          fontSize: '11px',
-          mr: '4px',
-          mb: 1,
-          color: alpha(common.black, 0.6),
-          [dark()]: {
-            color: alpha(common.white, 0.5),
-          },
-        }}
-      >
-        {formatTime(date)}
-      </Typography>
-      {clearBoth}
-      <Typography
-        sx={{
-          float: 'right',
-          display: 'inline-block',
-          minWidth: '2em',
-          maxWidth: '90%',
-          p: 1,
-          borderRadius: 1,
-          whiteSpace: 'pre-wrap',
-          wordBreak: 'break-all',
-          color: alpha(common.white, 0.9),
-          border: '1px solid',
-          boxShadow: `0 0 8px ${alpha(blue[700], 0.5)}`,
-          borderColor: alpha(blue[700], 0.5),
-          backgroundColor: alpha(blue[700], 0.75),
-          [dark()]: {
-            color: alpha(common.black, 0.75),
-            boxShadow: `0 0 8px ${alpha(common.white, 0.15)}`,
-            borderColor: alpha(common.white, 0.15),
-            backgroundColor: alpha(blue[200], 0.75),
-          },
-        }}
-      >
-        {text}
-      </Typography>
-      {clearBoth}
-    </Box>
-  )
-}
 
 export function MessageViewer() {
   const { peer, activeConnectionInfo } = usePeer()
@@ -157,11 +49,11 @@ export function MessageViewer() {
       {messageList.map((item) => {
         switch (item.type) {
           case 'text':
-            return <Text key={item.id} {...item} />
+            return <TextMessageElem key={item.id} {...item} />
           default:
             // 新增 "不支持的消息类型" 类型组件
             return (
-              <Text
+              <TextMessageElem
                 key={item.id}
                 {...item}
                 type='text'
