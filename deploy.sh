9d0215fc96a7e6bec93af90610f93c68f2a32f55
xiaomingTang <1038761793@qq.com>
2024-03-07 23:48:07 +0800

    fix: "pnpm server" instead of "node server.js"

--- deploy.sh
+++ deploy.sh
@@ -61,7 +61,7 @@ sshpass -p $P1_SSH_PASSWORD ssh -t $P1_SSH_USER@$P1_SSH_HOST "zsh -s $P1_REMOTE_
 
   pm2 delete $app_name
   log_file_name=".bak/.bak.log-$(date +%Y-%m-%d-%H-%M-%S).log"
-  pm2 start node --name $app_name --log $log_file_name -- server.js
+  pm2 start pnpm --name $app_name --log $log_file_name -- server
   # 限制 log 备份文件数量
   ls -at .bak/.bak.log-*.log | sed -n '30,$p' | xargs -I {} rm -rf {}
   # 限制 code 备份文件数量
