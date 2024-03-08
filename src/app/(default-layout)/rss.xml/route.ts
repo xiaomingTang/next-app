9f10202ac6541fd8320a48e9e21ea84c16cf8c4e
tangqiulin <tangqiulin@box3.ink>
2024-03-08 19:38:27 +0800

    perf: 使用 next/cache noStore 而非 header() 来控制避免页面静态

--- src/app/(default-layout)/rss.xml/route.ts
+++ src/app/(default-layout)/rss.xml/route.ts
@@ -5,32 +5,12 @@ import { getTags } from '@ADMIN/tag/server'
 import { privateGetBlogs } from '@/app/admin/blog/server'
 
 import showdown from 'showdown'
-import { headers } from 'next/headers'
-import { unstable_cache } from 'next/cache'
+import { unstable_cache, unstable_noStore as noStore } from 'next/cache'
 import RSS from 'rss'
 
 export async function GET() {
-  console.log('--- rss.xms ---')
-  // 跳过 build 阶段
-  if (process.env.npm_lifecycle_event === 'build') {
-    // 啥也不干, 只是为了避免 sitemap 变成纯静态文件
-    headers()
-    const feed = new RSS(
-      {
-        title: seo.title(),
-        description: seo.description(),
-        feed_url: resolvePath('/rss.xml').href,
-        site_url: resolvePath('/').href,
-        language: 'zh-CN',
-        copyright: `All rights reserved ${new Date().getFullYear()}`,
-        categories: [],
-        ttl: 60,
-      },
-      []
-    )
-
-    return new Response(feed.xml())
-  }
+  noStore()
+  console.log('--- rss.xml ---')
 
   const allBlogs = await privateGetBlogs({
     type: 'PUBLISHED',
