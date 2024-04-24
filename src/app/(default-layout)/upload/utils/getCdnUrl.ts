d28404f23eecdee06657544d19c552101ec8e5b7
tangqiulin <tangqiulin@box3.ink>
2024-04-24 16:31:59 +0800

    feat: rm finished "todo"

--- src/app/(default-layout)/upload/utils/getCdnUrl.ts
+++ src/app/(default-layout)/upload/utils/getCdnUrl.ts
@@ -1,7 +1,6 @@
 export function getCdnUrl(props?: { key?: string }) {
   const key = props?.key ?? '/'
 
-  // TODO: 改成 CDN 的值
   const url = new URL(process.env.NEXT_PUBLIC_CDN_ROOT)
   url.pathname = key
   return url
