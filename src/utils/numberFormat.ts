73cd3746fa0e1ae60c80d09892bb8c80730813a2
tangqiulin <tangqiulin@box3.ink>
2024-05-13 23:15:35 +0800

    perf: 优化数字格式化

--- src/utils/numberFormat.ts
+++ src/utils/numberFormat.ts
@@ -1,4 +1,11 @@
-export function numberFormat(val: number | string = '') {
+export function numberFormat(val?: number | string | null, defaultValue = 0) {
+  // +null 为 0, 需要提前在此判断
+  if (val === null || val === undefined) {
+    return defaultValue
+  }
+  if (typeof val === 'string' && val.trim() === '') {
+    return defaultValue
+  }
   const num = +val
-  return Number.isNaN(num) ? 0 : num
+  return Number.isNaN(num) ? defaultValue : num
 }
