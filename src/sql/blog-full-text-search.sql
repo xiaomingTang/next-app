5277198cf417aaf9123fa039b8afcc2c66d7651b
tangqiulin <tangqiulin@box3.ink>
2024-07-31 18:10:56 +0800

    fix: search sql 返回类型错误

--- src/sql/blog-full-text-search.sql
+++ src/sql/blog-full-text-search.sql
@@ -6,30 +6,33 @@
 
 SELECT
     JSON_OBJECT(
-        'id',
-        u.`id`,
-        'name',
-        u.`name`,
-        'role',
-        u.`role`
-    ) AS `user`,
-    b.`hash`,
-    b.`type`,
-    b.`createdAt`,
-    b.`updatedAt`,
-    b.`title`,
+        'id', u.id,
+        'name', u.name,
+        'role', u.role
+    ) AS creator,
+    b.hash,
+    b.type,
+    b.createdAt,
+    b.updatedAt,
+    b.title,
     -- 不需要查询 content 字段，只需要返回的时候带上这个字段
-    "" AS `content`,
-    b.`description`,
-    b.`creatorId`
+    "" AS content,
+    b.description,
+    JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'name', t.name,
+            'hash', t.hash,
+            'description', t.description
+        )
+    ) AS tags
 FROM `16px`.`Blog` AS b
-    JOIN `16px`.`User` AS u ON b.`creatorId` = u.`id`
-WHERE (
-        b.`type` = "PUBLISHED"
-        AND MATCH (
-            b.`title`,
-            b.`description`,
-            b.`content`
-        ) AGAINST (? IN NATURAL LANGUAGE MODE)
-    )
+JOIN `16px`.`User` AS u
+    ON b.creatorId = u.id
+LEFT JOIN `16px`.`_BlogToTag` AS bt
+    ON b.hash = bt.A
+LEFT JOIN `16px`.`Tag` AS t
+    ON bt.B = t.hash
+WHERE b.type = "PUBLISHED"
+  AND MATCH (b.title, b.description, b.content) AGAINST (? IN NATURAL LANGUAGE MODE)
+GROUP BY b.hash, b.type, b.createdAt, b.updatedAt, b.title, b.description, u.id, u.name, u.role
 LIMIT 5;
