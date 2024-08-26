edda5eaeb74e11fdec3efd163b6e1360afb28ce7
tangqiulin <tangqiulin@box3.ink>
2024-08-26 16:02:30 +0800

    perf: 留言页 ui

--- src/app/(default-layout)/comment/components/CommentArea.tsx
+++ src/app/(default-layout)/comment/components/CommentArea.tsx
@@ -7,12 +7,116 @@ import { cat } from '@/errors/catchAndToast'
 import { SA } from '@/errors/utils'
 import { useLoading } from '@/hooks/useLoading'
 
-import { Alert, TextField } from '@mui/material'
+import { Alert, Box, Stack, TextField } from '@mui/material'
 import { Controller, useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import { LoadingButton } from '@mui/lab'
 
+import type { Control } from 'react-hook-form'
+
 type SaveCommentProps = Parameters<typeof saveComment>[0]
+
+function ContentController({
+  control,
+}: {
+  control: Control<SaveCommentProps>
+}) {
+  return (
+    <Controller
+      name='content'
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          autoFocus
+          label='留言内容'
+          placeholder='仅站长可见'
+          multiline
+          sx={{ width: '100%' }}
+          helperText={error?.message ?? ' '}
+          error={!!error}
+          inputProps={{
+            style: {
+              overflow: 'auto',
+              height: '14em',
+            },
+          }}
+        />
+      )}
+      rules={{
+        required: {
+          value: true,
+          message: '必填项',
+        },
+        minLength: {
+          value: 2,
+          message: '多写一点吧',
+        },
+        maxLength: {
+          value: 10000,
+          message: '最多 10000 个字',
+        },
+      }}
+    />
+  )
+}
+
+function EmailController({ control }: { control: Control<SaveCommentProps> }) {
+  return (
+    <Controller
+      name='email'
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          sx={{ width: '100%' }}
+          label='邮箱 (可选)'
+          placeholder='留下邮箱, 我才能回复你'
+          helperText={error?.message ?? ' '}
+          error={!!error}
+        />
+      )}
+      rules={{
+        maxLength: {
+          value: 200,
+          message: '最多 200 个字符',
+        },
+        pattern: {
+          value: /^\S+@\S+$/i,
+          message: '无效邮箱',
+        },
+      }}
+    />
+  )
+}
+
+function NicknameController({
+  control,
+}: {
+  control: Control<SaveCommentProps>
+}) {
+  return (
+    <Controller
+      name='name'
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          sx={{ width: '100%' }}
+          label='怎么称呼你呢 (可选)'
+          helperText={error?.message ?? ' '}
+          error={!!error}
+        />
+      )}
+      rules={{
+        maxLength: {
+          value: 200,
+          message: '最多 200 个字符',
+        },
+      }}
+    />
+  )
+}
 
 export function CommentArea() {
   const [loading, withLoading] = useLoading()
@@ -34,90 +138,20 @@ export function CommentArea() {
           )
         )}
       >
-        <Controller
-          name='content'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <TextField
-              {...field}
-              autoFocus
-              label='留言内容'
-              placeholder='仅站长可见'
-              multiline
-              sx={{ width: '100%' }}
-              helperText={error?.message ?? ' '}
-              error={!!error}
-              inputProps={{
-                style: {
-                  overflow: 'auto',
-                  height: '14em',
-                },
-              }}
-            />
-          )}
-          rules={{
-            required: {
-              value: true,
-              message: '必填项',
-            },
-            minLength: {
-              value: 2,
-              message: '多写一点吧',
-            },
-            maxLength: {
-              value: 10000,
-              message: '最多 10000 个字',
-            },
-          }}
-        />
-        <Controller
-          name='email'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <TextField
-              {...field}
-              sx={{ width: '100%' }}
-              label='邮箱 (可选)'
-              placeholder='留下邮箱, 我才能回复你'
-              helperText={error?.message ?? ' '}
-              error={!!error}
-            />
-          )}
-          rules={{
-            maxLength: {
-              value: 200,
-              message: '最多 200 个字符',
-            },
-            pattern: {
-              value: /^\S+@\S+$/i,
-              message: '无效邮箱',
-            },
-          }}
-        />
-        <Controller
-          name='name'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <TextField
-              {...field}
-              sx={{ width: '100%' }}
-              label='怎么称呼你呢 (可选)'
-              helperText={error?.message ?? ' '}
-              error={!!error}
-            />
-          )}
-          rules={{
-            maxLength: {
-              value: 200,
-              message: '最多 200 个字符',
-            },
-          }}
-        />
+        <ContentController control={control} />
+        <Stack columnGap={2} direction='row' useFlexGap flexWrap='wrap'>
+          <Box width={{ xs: '100%', sm: '36%', md: '30%' }}>
+            <EmailController control={control} />
+          </Box>
+          <Box width={{ xs: '100%', sm: '36%', md: '30%' }}>
+            <NicknameController control={control} />
+          </Box>
+        </Stack>
         <LoadingButton
           loading={loading}
           variant='contained'
           type='submit'
-          sx={{ width: '100%' }}
+          sx={{ width: { xs: '100%', sm: '36%', md: '30%' } }}
         >
           提交
         </LoadingButton>
