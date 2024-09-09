bc2c3cfc205c1070aa75ab8fe9ea43c0d276d70d
xiaomingTang <1038761793@qq.com>
2024-09-10 01:43:02 +0800

    perf: dynamic import jsqr

--- src/app/(fullscreen-layout)/qrcode/scan/components/QrcodeScanner.tsx
+++ src/app/(fullscreen-layout)/qrcode/scan/components/QrcodeScanner.tsx
@@ -8,10 +8,9 @@ import { getUserMedia } from '@/utils/media'
 import { StreamVideo } from '@/app/(default-layout)/blog/components/StreamVideo'
 
 import useSWR from 'swr'
-import { useState } from 'react'
-import jsQR from 'jsqr'
+import { useMemo, useRef, useState } from 'react'
 import { toast } from 'react-hot-toast'
-import { Box } from '@mui/material'
+import { Box, CircularProgress } from '@mui/material'
 import { throttle } from 'lodash-es'
 
 import type { StreamVideoContext } from '@/app/(default-layout)/blog/components/StreamVideo'
@@ -22,10 +21,12 @@ export function QrcodeScanner({
 }: {
   fit?: 'cover' | 'contain'
 }) {
-  let readerTimeoutFlag = -1
+  const readerTimeoutFlagRef = useRef(-1)
   const { data: mediaStream = null } = useSWR('getUserVideo', () =>
     getUserMedia().catch((err) => {
-      toast.error(toPlainError(err).message)
+      toast.error(
+        `获取摄像头权限失败，请刷新页面以重试：${toPlainError(err).message}`
+      )
     })
   )
 
@@ -37,24 +38,38 @@ export function QrcodeScanner({
 
   useQrcodeHandler(QRContent?.data)
 
-  const onTick = throttle(({ ctx, canvas }: StreamVideoContext) => {
-    setSavedCanvasSize({
-      width: canvas.width,
-      height: canvas.height,
-    })
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-    const content = jsQR(imageData.data, canvas.width, canvas.height, {
-      inversionAttempts: 'attemptBoth',
-    })
-    if (content) {
-      setQRContent(content)
-      // 重置 timer
-      window.clearTimeout(readerTimeoutFlag)
-      readerTimeoutFlag = window.setTimeout(() => {
-        setQRContent(null)
-      }, 2500)
+  const { data: jsQR, isValidating } = useSWR('jsqr', () =>
+    import('jsqr')
+      .then((mod) => mod.default)
+      .catch(() => {
+        toast.error('组件加载错误，请刷新页面以重试')
+        return null
+      })
+  )
+
+  const onTick = useMemo(() => {
+    if (!jsQR) {
+      return () => undefined
     }
-  }, 500)
+    return throttle(({ ctx, canvas }: StreamVideoContext) => {
+      setSavedCanvasSize({
+        width: canvas.width,
+        height: canvas.height,
+      })
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+      const content = jsQR(imageData.data, canvas.width, canvas.height, {
+        inversionAttempts: 'attemptBoth',
+      })
+      if (content) {
+        setQRContent(content)
+        // 重置 timer
+        window.clearTimeout(readerTimeoutFlagRef.current)
+        readerTimeoutFlagRef.current = window.setTimeout(() => {
+          setQRContent(null)
+        }, 2500)
+      }
+    }, 500)
+  }, [jsQR])
 
   return (
     <Box
@@ -68,6 +83,17 @@ export function QrcodeScanner({
       <StreamVideo fit={fit} mediaStream={mediaStream} onTick={onTick} />
       {QRContent && (
         <QrcodeDisplayItem qrcode={QRContent} canvasSize={savedCanvasSize} />
+      )}
+      {isValidating && (
+        <CircularProgress
+          size='24px'
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        />
       )}
     </Box>
   )
