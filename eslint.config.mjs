fac9d7fac91d32d4b2156d203a75fbf67a0c507f
xiaomingTang <1038761793@qq.com>
2024-11-11 03:25:40 +0800

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,8 +21,10 @@ export default [
       '**/*.md',
       '**/*.mdx',
       '**/.next/',
+      '**/.local/',
       '**/out/',
       '**/public/',
+      '**/node_modules/',
     ],
   },
   ...compat.extends(
