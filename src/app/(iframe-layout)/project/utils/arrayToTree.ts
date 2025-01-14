db0aafc25a7efc4010d57f09afa26ba6aae99064
xiaomingTang <1038761793@qq.com>
2025-01-15 01:51:35 +0800

    feat: 移除没用到的类

--- src/app/(iframe-layout)/project/utils/arrayToTree.ts
+++ src/app/(iframe-layout)/project/utils/arrayToTree.ts
@@ -1,4 +1,3 @@
-import type { ContentType, ProjectType } from '@prisma/client'
 import type { getRootProjectMenu } from '../server'
 import type { SA_RES } from '@/errors/utils'
 
@@ -6,79 +5,6 @@ export type SimpleProjectItem = SA_RES<typeof getRootProjectMenu>[number]
 
 export interface ProjectTree extends SimpleProjectItem {
   children?: ProjectTree[]
-}
-
-export class Tree {
-  hash: string
-
-  type: ProjectType
-
-  createdAt: Date
-
-  updatedAt: Date
-
-  name: string
-
-  parentHash: string
-
-  contentType: ContentType | null
-
-  children?: Tree[]
-
-  parent?: Tree
-
-  get isRoot() {
-    return !this.parent
-  }
-
-  get root() {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    let root: Tree = this
-    while (root.parent) {
-      root = root.parent
-    }
-    return root
-  }
-
-  get relPath(): string {
-    if (!this.parent) {
-      return '/'
-    }
-    if (this.parent.isRoot) {
-      return `/${this.name}`
-    }
-    return `${this.parent.relPath}/${this.name}`
-  }
-
-  static findById(tree: Tree, id: string): Tree | null {
-    if (tree.hash === id) {
-      return tree
-    }
-    if (tree.children) {
-      for (let i = 0; i < tree.children.length; i += 1) {
-        const found = Tree.findById(tree.children[i], id)
-        if (found) {
-          return found
-        }
-      }
-    }
-    return null
-  }
-
-  constructor(rawTree: ProjectTree) {
-    this.hash = rawTree.hash
-    this.type = rawTree.type
-    this.createdAt = rawTree.createdAt
-    this.updatedAt = rawTree.updatedAt
-    this.name = rawTree.name
-    this.parentHash = rawTree.parentHash
-    this.contentType = rawTree.contentType
-    this.children = rawTree.children?.map((child) => {
-      const tree = new Tree(child)
-      tree.parent = this
-      return tree
-    })
-  }
 }
 
 export function arrayToTree(items: ProjectTree[]) {
