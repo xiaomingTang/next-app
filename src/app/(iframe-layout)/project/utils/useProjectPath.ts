7fe606343f7e7dddfcac5ad47d389df7325a7729
xiaomingTang <1038761793@qq.com>
2025-01-16 02:08:29 +0800

    feat: project path hook 支持传入 path 数组

--- src/app/(iframe-layout)/project/utils/useProjectPath.ts
+++ src/app/(iframe-layout)/project/utils/useProjectPath.ts
@@ -7,24 +7,39 @@ const useRawProjectPath = create(() => ({
   path: '',
 }))
 
+function formatPath(p: string | string[]) {
+  if (!p) {
+    return '/'
+  }
+  if (Array.isArray(p)) {
+    if (p.length === 0) {
+      return '/'
+    }
+    return `/${p.join('/')}`
+  }
+  return p
+}
+
 export const useProjectPath = withStatic(useRawProjectPath, {
   setRootHash(newRootHash: string) {
     useRawProjectPath.setState({ rootHash: newRootHash })
   },
-  replace(newPath: string) {
+  replace(newPath: string | string[]) {
     const { rootHash } = useRawProjectPath.getState()
     if (!rootHash) {
       throw new Error('Root hash not found')
     }
-    useRawProjectPath.setState({ path: newPath })
-    window.history.replaceState(null, '', `/project/${rootHash}${newPath}`)
+    const p = formatPath(newPath)
+    useRawProjectPath.setState({ path: p })
+    window.history.replaceState(null, '', `/project/${rootHash}${p}`)
   },
-  push(newPath: string) {
+  push(newPath: string | string[]) {
     const { rootHash } = useRawProjectPath.getState()
     if (!rootHash) {
       throw new Error('Root hash not found')
     }
-    useRawProjectPath.setState({ path: newPath })
-    window.history.pushState(null, '', `/project/${rootHash}${newPath}`)
+    const p = formatPath(newPath)
+    useRawProjectPath.setState({ path: p })
+    window.history.pushState(null, '', `/project/${rootHash}${p}`)
   },
 })
