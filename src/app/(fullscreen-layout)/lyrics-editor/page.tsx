c9db91a0af8529f7b20e96124950955302c44f7a
tangqiulin <tangqiulin@box3.ink>
2025-02-12 10:57:02 +0800

    feat: update keywords

--- src/app/(fullscreen-layout)/lyrics-editor/page.tsx
+++ src/app/(fullscreen-layout)/lyrics-editor/page.tsx
@@ -4,7 +4,8 @@ import { seo } from '@/utils/seo'
 
 export const metadata = seo.defaults({
   title: '歌词编辑器',
-  keywords: '歌词,歌词编辑器,lrc,lyric,lyrics,lyrics editor',
+  keywords:
+    '歌词,歌词编辑器,lrc,lyric,lyrics,lyrics editor,拖拽编辑,波形图,音频,可视化,可视化编辑',
 })
 
 export default function Index() {
