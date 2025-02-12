dc3798fcea939a506f0e07592a026213da35ca90
tangqiulin <tangqiulin@box3.ink>
2025-02-12 10:57:40 +0800

    feat: lyrics-editor 添加到 sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -105,6 +105,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.6,
     },
     {
+      url: resolvePath('/lyrics-editor').href,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
       url: resolvePath('/qrcode/generate').href,
       lastModified: new Date(),
       changeFrequency: 'yearly',
