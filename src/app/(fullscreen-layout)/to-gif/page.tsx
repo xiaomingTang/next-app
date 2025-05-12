eb76cae63347bcbc05e282812221a04f8e63e866
xiaomingTang <1038761793@qq.com>
2025-05-12 15:05:25 +0800

    feat: 更新页面 description & 新增 keywords

--- src/app/(fullscreen-layout)/to-gif/page.tsx
+++ src/app/(fullscreen-layout)/to-gif/page.tsx
@@ -4,7 +4,8 @@ import { seo } from '@/utils/seo'
 
 export const metadata = seo.defaults({
   title: '图片转gif',
-  description: '图片转gif,jpg转gif,png转gif,图片转动图,',
+  description: '纯前端的图片转gif工具，支持多种图片格式合并为gif',
+  keywords: '图片转gif,jpg转gif,png转gif,图片转动图',
 })
 
 export default function Index() {
