ab664c3bf721b9a89692430acb87d81959e37be4
xiaomingTang <1038761793@qq.com>
2025-05-14 21:09:49 +0800

    feat: 添加 next-image 站内图片优化

--- next-image.config.js
+++ next-image.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next/dist/shared/lib/image-config').ImageConfig} */
 module.exports = {
   minimumCacheTTL: 60 * 60,
-  remotePatterns: [],
+  domains: ['cdn.16px.cc', '16px.cc'],
+  remotePatterns: [
+    new URL('https://cdn.16px.cc/**'),
+    new URL('https://16px.cc/**'),
+  ],
 }
