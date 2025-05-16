458eaf6b4cf1239e96e6231078d720ec3e11d1ca
xiaomingTang <1038761793@qq.com>
2025-05-17 03:10:12 +0800

    feat: 新增入口及 sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -63,6 +63,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...blogRoutes,
     ...tagRoutes,
     {
+      url: resolvePath('/sh/ffmpeg').href,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
       url: resolvePath('/to-gif').href,
       lastModified: new Date(),
       changeFrequency: 'yearly',
