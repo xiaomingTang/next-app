aef71256d9bdd60c9ba7255bde868836c82b36ac
xiaomingTang <1038761793@qq.com>
2025-06-05 05:41:43 +0800

    fix: 原来是我把 title 放到 layout 里面了，loading 页忘了改

--- src/app/(default-layout)/tts/task/[hash]/loading.tsx
+++ src/app/(default-layout)/tts/task/[hash]/loading.tsx
@@ -1,24 +1,10 @@
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
 import CircularProgress from '@mui/material/CircularProgress'
 
 export default function LoadingPage() {
   return (
-    <>
-      <Typography
-        variant='h5'
-        sx={{ textAlign: 'center', fontWeight: 'bold', mb: 2 }}
-      >
-        TTS 任务详情
-      </Typography>
-      <Box
-        display='flex'
-        justifyContent='center'
-        alignItems='center'
-        minHeight={200}
-      >
-        <CircularProgress />
-      </Box>
-    </>
+    <Box display='flex' justifyContent='center' alignItems='center'>
+      <CircularProgress />
+    </Box>
   )
 }
