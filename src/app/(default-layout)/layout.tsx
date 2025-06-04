1bd16230cf3b69f4022eff8c983f06c0be145373
xiaomingTang <1038761793@qq.com>
2025-06-05 03:27:23 +0800

    feat: tts 页移除 侧边时钟

--- src/app/(default-layout)/layout.tsx
+++ src/app/(default-layout)/layout.tsx
@@ -1,22 +1,12 @@
 import { GA } from '@/analytics/GA'
-import { DefaultAside } from '@/layout/DefaultAside'
 import { DefaultBodyContainer } from '@/layout/DefaultBodyContainer'
 import DefaultLayout from '@/layout/DefaultLayout'
-
-import dynamic from 'next/dynamic'
-
-const DefaultClock = dynamic(() =>
-  import('@D/clock/components/DefaultClock').then((res) => res.DefaultClock)
-)
 
 export default function Index({ children }: { children: React.ReactNode }) {
   return (
     <DefaultLayout>
       <DefaultBodyContainer>
         <GA />
-        <DefaultAside placement='left'>
-          <DefaultClock />
-        </DefaultAside>
         {children}
       </DefaultBodyContainer>
     </DefaultLayout>
