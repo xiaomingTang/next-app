5537f185cbe6ad7e697422693143028794128fb0
xiaomingTang <1038761793@qq.com>
2025-06-05 03:03:24 +0800

    perf: tts options decoder 入参类型放松

--- src/app/(default-layout)/tts/utils/ttsOptionParser.ts
+++ src/app/(default-layout)/tts/utils/ttsOptionParser.ts
@@ -1,7 +1,20 @@
 import type { TtsMergeOption } from './tts'
 
+function decode(arg: { options: string }): TtsMergeOption
+function decode(arg?: undefined | void | null): null
+function decode(
+  arg?: { options: string } | undefined | void | null
+): TtsMergeOption | null
+function decode(
+  arg?: { options: string } | undefined | void | null
+): TtsMergeOption | null {
+  if (!arg?.options) {
+    return null
+  }
+  return JSON.parse(arg.options) as TtsMergeOption
+}
+
 export const ttsOptionParser = {
   encode: (arg: TtsMergeOption) => JSON.stringify(arg),
-  decode: (arg: { options: string }) =>
-    JSON.parse(arg.options) as TtsMergeOption,
+  decode,
 }
