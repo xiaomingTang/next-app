edac5c5ce1af4f4ddc66edf2ad170806aa4832d1
xiaomingTang <1038761793@qq.com>
2025-06-05 05:28:48 +0800

    fix: error page 在 layout 里面

--- src/app/(default-layout)/tts/task/[hash]/error.tsx
+++ src/app/(default-layout)/tts/task/[hash]/error.tsx
@@ -7,31 +7,24 @@ import { toPlainError } from '@/errors/utils'
 
 import { Alert, Typography } from '@mui/material'
 
+import type { PlainError } from '@/errors/utils'
+
 export default function ErrorPage({
   error: rawError,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: PlainError | Error
 }) {
   const error = toPlainError(rawError)
 
   return (
-    <>
-      <Typography
-        variant='h5'
-        sx={{ textAlign: 'center', fontWeight: 'bold', mb: 2 }}
-      >
-        TTS 任务详情
+    <Alert severity='error'>
+      <Typography sx={{ mb: 1 }}>{error.message}</Typography>
+      <Typography>
+        <Typography component='span'>{'你还可以 '}</Typography>
+        <Anchor href='/tts/tasks'>返回任务列表</Anchor>
+        {' 或者 '}
+        <Anchor href='/tts'>再新建一个任务</Anchor>
       </Typography>
-      <Alert severity='error'>
-        <Typography sx={{ mb: 1 }}>{error.message}</Typography>
-        <Typography>
-          <Typography component='span'>{'你还可以 '}</Typography>
-          <Anchor href='/tts/tasks'>返回任务列表</Anchor>
-          {' 或者 '}
-          <Anchor href='/tts'>再新建一个任务</Anchor>
-        </Typography>
-      </Alert>
-    </>
+    </Alert>
   )
 }
