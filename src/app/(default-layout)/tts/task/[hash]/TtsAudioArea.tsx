282153e745c38a0162b994155f7fec33561f23b6
xiaomingTang <1038761793@qq.com>
2025-06-05 14:56:15 +0800

    feat: 新增下载提示

--- src/app/(default-layout)/tts/task/[hash]/TtsAudioArea.tsx
+++ src/app/(default-layout)/tts/task/[hash]/TtsAudioArea.tsx
@@ -6,6 +6,7 @@ import { getCdnUrl } from '@/app/(default-layout)/upload/utils/getCdnUrl'
 import { Delay } from '@/components/Delay'
 
 import { useMemo } from 'react'
+import { Alert, Divider } from '@mui/material'
 
 import type { TtsTaskItem } from '../../server'
 
@@ -21,6 +22,10 @@ export function TtsAudioArea({ task: initTask }: { task: TtsTaskItem }) {
     // 任务成功，直接返回音频
     return (
       <Delay delayMs={1000}>
+        <Divider />
+        <Alert severity='warning'>
+          请尽快下载音频文件，因为历史记录及文件会不定期清理。
+        </Alert>
         <audio src={url} controls style={{ width: '100%' }} />
       </Delay>
     )
