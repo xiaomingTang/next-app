3fdebba9a6f52a341fa1bd099c5abf070d3781c2
xiaomingTang <1038761793@qq.com>
2025-06-06 21:56:35 +0800

    feat: 优化 usehover 在触摸设备上的表现

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,3 +1,5 @@
+import { touchable } from '@/utils/device'
+
 import { noop } from 'lodash-es'
 import { useState, useEffect } from 'react'
 
@@ -11,13 +13,26 @@ export function useHover() {
     }
     const onEnter = () => setHovered(true)
     const onLeave = () => setHovered(false)
-    elem.addEventListener('pointerenter', onEnter)
-    elem.addEventListener('pointerleave', onLeave)
+    const onTouchOut = (e: TouchEvent) => {
+      if (!elem.contains(e.target as Node)) {
+        setHovered(false)
+      }
+    }
+
+    if (touchable) {
+      elem.addEventListener('touchstart', onEnter, { passive: true })
+      window.addEventListener('touchstart', onTouchOut, { passive: true })
+    } else {
+      elem.addEventListener('mouseenter', onEnter, { passive: true })
+      elem.addEventListener('mouseleave', onLeave, { passive: true })
+    }
     window.addEventListener('pointercancel', onLeave)
 
     return () => {
-      elem.removeEventListener('pointerenter', onEnter)
-      elem.removeEventListener('pointerleave', onLeave)
+      elem.removeEventListener('touchstart', onEnter)
+      window.removeEventListener('touchstart', onTouchOut)
+      elem.removeEventListener('mouseenter', onEnter)
+      elem.removeEventListener('mouseleave', onLeave)
       window.removeEventListener('pointercancel', onLeave)
     }
   }, [elem])
